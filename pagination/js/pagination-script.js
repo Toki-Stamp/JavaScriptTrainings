052a996ca8ca87892fc0667ebb5f0a2736b6e969
Fomichev Yuri <fomichev@nca.by>
2017-10-23 16:46:37 +0300

    23.10.2017 Оптимизация

--- pagination/js/pagination-script.js
+++ pagination/js/pagination-script.js
@@ -5,10 +5,11 @@
 jQuery(document).ready(function main() {
     /* -------------------------------------------------- Переменные */
     var $pagination = $('#pagination-container');
-    var activeClass = 'btn-success', defaultClass = 'btn-default';
+    var $navigation;
+    var $pages;
+    var $all;
     /* -------------------------------------------------- Константы */
-    const currentButtonAttributeName = 'btn-current-val';
-    const previousButtonAttributeName = 'btn-previous-val';
+    const activeClass = 'btn-primary', defaultClass = 'btn-default';
     /* -------------------------------------------------- Полезные функции */
     /* Классный логер в консоль */
     var log = function (target, message) {
@@ -18,57 +19,127 @@ jQuery(document).ready(function main() {
             console.dir(target);
         }
     };
-    /* Устанавливаем класс активной кнопки */
-    var setActive = function (setClass, removeClass) {
-        var $currentVal = $pagination.attr(currentButtonAttributeName) ? $pagination.attr(currentButtonAttributeName) : 1;
-        var $previousVal = $pagination.attr(previousButtonAttributeName);
-        var $currentButton = $pagination.find('input[type="button"][value="' + $currentVal + '"]');
-        var $previousButton = $previousVal ? $pagination.find('input[type="button"][value="' + $previousVal + '"]') : undefined;
-        activeClass = setClass ? setClass : activeClass;
-        defaultClass = removeClass ? removeClass : defaultClass;
-        if ($currentVal) $currentButton.addClass(activeClass).removeClass(defaultClass);
-        if ($previousVal) $previousButton.addClass(defaultClass).removeClass(activeClass);
-    };
-    /* Устанавливаем необходимое свойство */
-    var setProperty = function (target, property, value) {
-        if (value) {
-            /* Устанавыливаем */
-            target.prop(property, value);
-        } else {
-            /* Переключаем */
-            target.prop(property, function (index, oldValue) {
-                /* Игнорируем параметр index */
-                return !oldValue;
-            });
+    /* Устанавливаем активную кнопку */
+    var setActive = function (target) {
+        var $active = findActive();
+        if ($active) {
+            $active.addClass(defaultClass).removeClass(activeClass);
+            trigger($navigation, true);
+        }
+        if (target) {
+            target.addClass(activeClass).removeClass(defaultClass);
+            if (target.val() === $pages.first().val()) {
+                trigger($navigation.first(), false);
+            } else if (target.val() === $pages.last().val()) {
+                trigger($navigation.last(), false);
+            } else {
+                trigger($navigation, true);
+            }
         }
     };
-    /* -------------------------------------------------- Main */
-    setActive();
-    /* -------------------------------------------------- Обработчики */
-    /* Подсвечиваем активную страницу */
-    $pagination.find('input[type="button"]').on('click', function () {
-        var $self = $(this);
-        if (!$self.hasClass('disabled') || !$self.hasClass(activeClass)) setActive();
-    });
-    /* Стрелка влево / вправо */
-    $pagination.find('button')
-        .on('mouseup', function () {
-        })
-        .on('dblclick', function () {
-        });
-    /* Все кнопки панели постраничного отображения */
-    $pagination.find('input[type="button"], button')
-        .on('mousedown', function () {
-            var $self = $(this),
-                $parent = $self.parent(),
-                currentVal = $self.val(),
-                previousVal = $parent.attr(currentButtonAttributeName) ? $parent.attr(currentButtonAttributeName) : 1;
-            if (currentVal !== previousVal) {
-                $parent.attr(currentButtonAttributeName, currentVal);
-                $parent.attr(previousButtonAttributeName, previousVal);
+    /* Ищем активную кнопку */
+    var findActive = function () {
+        var $active = $pages.filter('.' + activeClass);
+        return $active.length ? $active : undefined;
+    };
+    /* Ищем следующую кнопку */
+    var findNext = function (direction) {
+        var $active = findActive(),
+            $first = $pages ? $pages.first() : undefined,
+            $last = $pages ? $pages.last() : undefined,
+            $next,
+            filter = 'input[type="button"]';
+        if ($pages && direction) {
+            switch (direction) {
+                case 'forward':
+                    if ($active.val() !== $last.val()) {
+                        $next = $active.next(filter);
+                    }
+                    break;
+                case 'backward':
+                    if ($active.val() !== $first.val()) {
+                        $next = $active.prev(filter);
+                    }
+                    break;
             }
-        })
-        .on('mouseup', function () {
+        }
+        return $next;
+    };
+    /* Включаем / выключаем функциональность навигационных стрелок */
+    var trigger = function (target, value) {
+        if (target) {
+            target.prop('disabled', !value);
+        }
+    };
+    /* Строим панель постраничной навигации */
+    var buildPagination = function (pages) {
+        /* Рисуем навигационную кнопку */
+        var addNavigationButton = function (target, size, value, glyphicon) {
+            if (target && size && size > 1) {
+                target.append(
+                    '<button class="btn btn-default" value="' + value + '">' +
+                    '<span class="glyphicon ' + glyphicon + '"></span>' +
+                    '</button>'
+                );
+            }
+        };
+        /* Рисуем страницу */
+        var addPages = function (target, size) {
+            if (target && size) {
+                for (var i = 1; i <= size; i++) {
+                    target.append(
+                        '<input type="button" class="btn btn-default" value="' + i + '"/>'
+                    );
+                }
+            }
+        };
+        addNavigationButton($pagination, pages, 'backward', 'glyphicon-hand-left');
+        addPages($pagination, pages);
+        addNavigationButton($pagination, pages, 'forward', 'glyphicon-hand-right');
+    };
+    /* Инициализация панели постраничного отображения */
+    var initialize = function (pages) {
+        /* -------------------------------------------------- Генерируем панель навигации */
+        buildPagination(pages);
+        /* -------------------------------------------------- Назначаем переменные */
+        $navigation = $pagination.find('button');
+        $pages = $pagination.find('input[type="button"]');
+        $all = $pagination.find('input[type="button"], button');
+        /* -------------------------------------------------- Создаём обработчики событий */
+        /* Подсвечиваем активную страницу */
+        $pages.on('click', function () {
+            setActive($(this));
+            log($(this).val());
+        });
+        /* Приячем фокус */
+        $all.on('click mouseleave mouseout', function () {
             $(this).blur();
         });
+        /* Двигаем активную страницу вперёд / назад */
+        $navigation.on('click', function () {
+            var $self = $(this),
+                direction = $self.val(),
+                $next = findNext(direction);
+            if ($next) {
+                setActive($next);
+            }
+        });
+        /* -------------------------------------------------- Выделяем первую активную страницу */
+        setActive($pages.first());
+    };
+    /* Очистка панели постраничного отображения */
+    var destroy = function () {
+        $pagination.empty();
+    };
+    /* -------------------------------------------------- Main */
+    initialize(10);
+    /* -------------------------------------------------- Обработчики */
+    /* Обработчик нажатий на клавиатуру */
+    $('body').on('keyup', function (e) {
+        if (e.keyCode == 27) {
+            destroy();
+        } else if (e.keyCode == 13) {
+            initialize(7);
+        }
+    });
 });
