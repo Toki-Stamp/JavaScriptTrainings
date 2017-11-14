0ecfba7640cec0263cba23ffbd7dbec8c84f4081
Yuri Fomichev <fomichev@nca.by>
2017-11-14 17:29:35 +0300

    Added css custom font

--- projects/replace/js/replace-script.js
+++ projects/replace/js/replace-script.js
@@ -4,39 +4,91 @@
  */
 jQuery(document).ready(function main() {
 
+    $('#input-normal').keydown(function (e) {
+        if (repeated(e.originalEvent)) return false;
+    }).keyup(function () {
+        autoReplace($(this));
+    });
+
     $('#input-advanced').keydown(function (e) {
-        if (repeatedEvent(e)) return false;
+        var self, parent;
+        if (repeated(e.originalEvent)) return false;
         if (e && (e.key === ',')) {
-            var self = $('#input-advanced'),
-                parent = self.parent();
+            self = $(this);
+            parent = self.parent().parent();
             self.next().addClass('glyphicon-pencil');
             parent.addClass('has-warning');
         }
     }).keyup(function () {
         var self = $(this),
-            parent = self.parent(),
-            replaceValue = self.val();
+            parent = self.parent().parent();
         if (parent.hasClass('has-warning')) {
             self.next().removeClass('glyphicon-pencil');
             parent.removeClass('has-warning');
         }
-        replaceValue = replaceValue.replace(/,/g, '.');
-        self.val(replaceValue);
+        autoReplace(self);
     });
-
-    $('#input-normal').keyup(function () {
-        var self = $(this),
-            replaceValue = self.val();
-        replaceValue = replaceValue.replace(/,/g, '.');
-        self.val(replaceValue);
-    });
-
-    function repeatedEvent(event) {
-        /* 8 - backspace / 37 - left arrow / 39 - right arrow / 46 - delete */
-        if (event && event.originalEvent.repeat) {
-            if (![8, 37, 39, 46].includes(event.keyCode)) {
-                return true;
-            }
-        }
-    }
 });
+
+$(document).keydown(function () {
+    $('#input-generated').keydown(function (e) {
+        var self, html;
+        if (repeated(e.originalEvent)) return false;
+        if (e && (e.key === ',')) {
+            self = $(this);
+            html = '<div id="input-advanced" class="form-group has-feedback">';
+            html += '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">';
+            html += '<input type="text" class="form-control auto-replace" title="Грациозно замещаем запятые!">';
+            html += '<span class="glyphicon form-control-feedback"></span>';
+            html += '</div>';
+            html += '</div>';
+            self.replaceWith(html);
+        }
+    });
+
+    $('#input-generated').keyup(function () {
+        var self = $(this);
+        console.log(self);
+        self.replaceWith('<input type="text" class="form-control" id="input-generated" title="Замещаем запятые и генерируем всю крутость вокруг">');
+        autoReplace($(this));
+    });
+});
+
+/**
+ * Получает событие нажатия клавиатуры и анализирует является ли переданное
+ * событие повторным, исключая те нажатия кнопок keyCodes,
+ * которые переданы в массиве except
+ *
+ * @param {Object} event - событие клавиатуры
+ * @param {Array} [except] - массив кнопок, которые игнорируются при анализе
+ * @returns {Boolean} - возвращает true если повторяемое событие игнорируется по keyCode
+ */
+function repeated(event, except) {
+    /* 8 - backspace / 37 - left arrow / 39 - right arrow / 46 - delete */
+    except = except || [8, 37, 39, 46];
+    if (event && event.repeat) {
+        return (except.indexOf(event.keyCode) === -1);
+    }
+    return false;
+}
+
+/**
+ * Находит в целевом элементе совпадения с символом find
+ * и замещает его символом replace в случае нахождения
+ *
+ * @param {Element} target - целевой элемент, к примеру input
+ * @param {String} [find] - символ для поиска
+ * @param {String} [replace] - символ на замену найденного
+ */
+function autoReplace(target, find, replace) {
+    var value,
+        pattern;
+    if (target) {
+        find = find || ',';
+        replace = replace || '.';
+        pattern = new RegExp(find, 'g');
+        value = target.val();
+        value = value.replace(pattern, replace);
+        target.val(value);
+    }
+}
