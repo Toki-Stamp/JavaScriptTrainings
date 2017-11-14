665d13ad88f61184ba4a3bd4d79b19acdfac4f6e
fomichev <fomichev@nca.by>
2017-11-15 00:35:40 +0300

    Чуток подпилил поиск инпутов Немного доработал template

--- _template/js/script.js
+++ _template/js/script.js
@@ -2,5 +2,10 @@
  * Created by Fomichev Yuri on 19.10.2017
  * Contact me at : toki.stamp@gmail.com
  */
+
 jQuery(document).ready(function main() {
 });
+
+/* Self-invoking function */
+(function main() {
+})();
--- projects/replace/js/replace-script.js
+++ projects/replace/js/replace-script.js
@@ -4,6 +4,8 @@
  */
 jQuery(document).ready(function main() {
 
+    console.log('jQuery here!');
+
     $('#input-normal').keydown(function (e) {
         if (repeated(e.originalEvent)) return false;
     }).keyup(function () {
@@ -29,26 +31,35 @@ jQuery(document).ready(function main() {
         autoReplace(self);
     });
 
-    var target = $('input').filter(function () {
-        return ($(this).attr('title') === 'Попадает');
-    });
-    target.addClass('auto-replace');
-    target.parent().parent().find('label').text(target.attr('title'));
-    target.on('click', function () {
-        console.log('this', this);
-        console.log('parent', $(this).parent()[0]);
-        console.log('parent-parent', $(this).parent().parent()[0]);
-    });
+    console.log('jQuery done!');
+
 });
 
+/* Self-invoking function */
+(function main() {
+
+    console.log('Self-invoking function here!');
+
+    $('input').filter(template)
+        .addClass('auto-replace')
+        .keydown(function (e) {
+            if (repeated(e.originalEvent)) return false;
+        })
+        .keyup(function () {
+            autoReplace($(this));
+        });
+
+    console.log('Self-invoking function done!')
+
+})();
+
 /**
- * Получает событие нажатия клавиатуры и анализирует является ли переданное
- * событие повторным, исключая те нажатия кнопок keyCodes,
- * которые переданы в массиве except
+ * Получает событие нажатия клавиатуры и анализирует является ли переданное событие
+ * повторным, исключая те нажатия кнопок keyCodes, которые переданы в массиве except
  *
  * @param {Object} event - событие клавиатуры
- * @param {Array} [except] - массив кнопок, которые игнорируются при анализе
- * @returns {Boolean} - возвращает true если повторяемое событие игнорируется по keyCode
+ * @param {Array} [except] - массив кнопок keyCodes, которые игнорируются
+ * @returns {Boolean} - возвращает true если событие игнорируется по keyCode
  */
 function repeated(event, except) {
     /* 8 - backspace / 37 - left arrow / 39 - right arrow / 46 - delete */
@@ -61,9 +72,9 @@ function repeated(event, except) {
 
 /**
  * Находит в целевом элементе совпадения с символом find
- * и замещает его символом replace в случае нахождения
+ * и замещает его символом replace если последний найден
  *
- * @param {Element} target - целевой элемент, к примеру input
+ * @param {Element} target - целевой элемент (к примеру input)
  * @param {String} [find] - символ для поиска
  * @param {String} [replace] - символ на замену найденного
  */
@@ -78,4 +89,17 @@ function autoReplace(target, find, replace) {
         value = value.replace(pattern, replace);
         target.val(value);
     }
+}
+
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function template() {
+    var text = this.attributes['placeholder'].value,
+        rnd = random(1, 3),
+        pattern = new RegExp(String(rnd), 'g'),
+        match = text.match(pattern);
+    console.log(text, (match ? 'подходит' : 'не подходит'), rnd);
+    return match;
 }
