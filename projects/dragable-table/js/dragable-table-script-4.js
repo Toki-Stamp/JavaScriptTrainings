1b99e218c23fc29c4ede640f8c9b422943965ec4
Yuri Fomichev <fomichev@nca.by>
2017-12-08 17:29:06 +0300

    tbody

--- projects/dragable-table/js/dragable-table-script-4.js
+++ projects/dragable-table/js/dragable-table-script-4.js
@@ -5,16 +5,59 @@
 
 /* jQuery */
 jQuery(document).ready(function main() {
-    var target = $('#main-table').find('tbody');
-    target.on('click', 'tr', function () {
-        var me = $(this);
+    var tbodies        = $('#main-table').find('tbody'),
+        veryFirstIndex = tbodies.find('tr').first().data('row-id') - 1,
+        veryLastIndex  = tbodies.find('tr').last().data('row-id'),
+        first,
+        firstIndex,
+        last,
+        lastIndex;
+    var total          = tbodies.find('tr:first');
+    var rows           = tbodies.find('tr:gt(0)');
 
-        /* split() */
+    // total.css('background-color', 'blue');
+    // rows.css('background-color', 'green');
 
-        console.log(target);
-        target.each(function () {
-            console.log(this)
-        });
-        console.log('data-row-id', me.data('row-id'));
+    tbodies.on('click', 'tr', function () {
+        // var me           = $(this),
+        //     currentIndex = me.data('row-id'),
+        //     rows         = tbodies.find('tr'),
+        //     rowsClone    = tbodies.find('tr').clone();
+
+        // var left    = rows.slice(veryFirstIndex, currentIndex),
+        //     right   = rows.slice(currentIndex, veryLastIndex),
+        //     indexes = [];
+        // rows.each(function () {
+        //     indexes.push($(this).data('row-id'));
+        // });
+        // console.log('    all:', indexes);
+        // indexes = [];
+        // left.each(function () {
+        //     indexes.push($(this).data('row-id'));
+        // });
+        // console.log('   left:', indexes);
+        // indexes = [];
+        // console.log('current:', currentIndex);
+        // if (currentIndex !== veryLastIndex) {
+        //     right.each(function () {
+        //         indexes.push($(this).data('row-id'));
+        //     });
+        // }
+        // console.log('  right:', indexes);
+        // console.log('--------------------------------------------');
+        // console.log('rows', rows);
+        // console.log('parent', rows.parent());
+        // console.log('closest', rows.closest('tbody'));
+        // left.wrapAll('<tbody></tbody>');
+        // right.wrapAll('<tbody></tbody>');
+        // tbodies.each(function () {
+        //     console.log(this);
+        //     rows       = $(this).find('tr');
+        //     first      = rows.first();
+        //     firstIndex = first.data('row-id');
+        //     last       = rows.last();
+        //     lastIndex  = last.data('row-id');
+        //     console.log('very-first-row-id', veryFirstIndex, 'first-row-id:', firstIndex, 'current-row-id:', current, 'last-row-id:', lastIndex, 'very-last-row-id', veryLastIndex);
+        // });
     });
 });
