eebdcdd463071687718b9f7deba71a8c863ac456
Yuri Fomichev <fomichev@nca.by>
2017-12-11 17:07:40 +0300

    конечный автомат

--- projects/dragable-table/js/dragable-table-script-4.js
+++ projects/dragable-table/js/dragable-table-script-4.js
@@ -5,66 +5,132 @@
 
 /* jQuery */
 jQuery(document).ready(function main() {
-    var tbodies        = $('#main-table').find('tbody'),
-        veryFirstIndex = tbodies.find('tr').first().data('row-id') - 1,
-        veryLastIndex  = tbodies.find('tr').last().data('row-id'),
-        firstIndex,
-        last,
-        lastIndex;
-    var first          = tbodies.find('tr:first');
-    var rows           = tbodies.find('tr:gt(0)');
 
-    first.css({
-        'border-color': 'green',
-        'border-style': 'solid',
-        'border-width': '2px'
-    });
-    rows.css({
-        'border-color': 'red',
-        'border-style': 'solid',
-        'border-width': '2px'
-    });
+    var input   = $('#inp-1'),
+        table   = $('#main-table'),
+        tbodies = table.find('tbody'),
+        tbody   = $('#main-table-tbody'),
+        total   = tbodies.find('tr:first'),
+        rows    = tbodies.find('tr:gt(0)'),
+        rows2   = rows.clone(),
+        valid   = function (index) {
+            var result = false;
+            if (index) {
+                if (typeof index !== 'number') {
+                    index = parseInt(index, 10);
+                }
+                result = ((veryFirstIndex <= index) && (index <= veryLastIndex));
+            }
+            return result;
+        };
 
-    tbodies.on('click', 'tr', function () {
-        // var me           = $(this),
-        //     currentIndex = me.data('row-id'),
-        //     rows         = tbodies.find('tr'),
-        //     rowsClone    = tbodies.find('tr').clone();
+    input.attr('placeholder', 'Row-index [' + rows.first().data('row-id') + '...' + rows.last().data('row-id') + ']');
 
-        // var left    = rows.slice(veryFirstIndex, currentIndex),
-        //     right   = rows.slice(currentIndex, veryLastIndex),
-        //     indexes = [];
-        // rows.each(function () {
-        //     indexes.push($(this).data('row-id'));
-        // });
-        // console.log('    all:', indexes);
-        // indexes = [];
-        // left.each(function () {
-        //     indexes.push($(this).data('row-id'));
-        // });
-        // console.log('   left:', indexes);
-        // indexes = [];
-        // console.log('current:', currentIndex);
-        // if (currentIndex !== veryLastIndex) {
-        //     right.each(function () {
-        //         indexes.push($(this).data('row-id'));
-        //     });
+    $('#btn-1').on('click', function () {
+        // var index = parseInt(input.val(), 10);
+        // var before, current, after;
+        // var tbodies;
+        // if (valid(index)) {
+        //     before  = rows.slice(veryFirstIndex - 1, index - 1);
+        //     current = rows.filter('[data-row-id="' + index + '"]');
+        //     after   = rows.slice(index, veryLastIndex);
+        //     rows.remove();
+        //     tbody.attr('data-body-type', 'total');
+        // tbody.after('<tbody data-body-type="after"></tbody>');
+        // tbody.after('<tbody class="fifo" data-body-type="current"></tbody>');
+        // tbody.after('<tbody data-body-type="before"></tbody>');
+        // tbodies = table.find('tbody');
+        // tbodies.filter('[data-body-type="before"]').append(before);
+        // tbodies.filter('[data-body-type="current"]').append(current);
+        // tbodies.filter('[data-body-type="after"]').append(after);
         // }
-        // console.log('  right:', indexes);
-        // console.log('--------------------------------------------');
-        // console.log('rows', rows);
-        // console.log('parent', rows.parent());
-        // console.log('closest', rows.closest('tbody'));
-        // left.wrapAll('<tbody></tbody>');
-        // right.wrapAll('<tbody></tbody>');
-        // tbodies.each(function () {
+        // input.val('');
+
+        var veryFirstIndex = rows.first().data('row-id'),
+            veryLastIndex  = rows.last().data('row-id');
+        // var success        = $('tr.success');
+        // var normal         = rows.filter(':not(.success)');
+        // if (success.length) {
+        //     success.each(function () {
+        //         console.log(this);
+        //     });
+        //     console.log('---------------------------------');
+        // }
+        // normal.each(function () {
         //     console.log(this);
-        //     rows       = $(this).find('tr');
-        //     first      = rows.first();
-        //     firstIndex = first.data('row-id');
-        //     last       = rows.last();
-        //     lastIndex  = last.data('row-id');
-        //     console.log('very-first-row-id', veryFirstIndex, 'first-row-id:', firstIndex, 'current-row-id:', current, 'last-row-id:', lastIndex, 'very-last-row-id', veryLastIndex);
         // });
+        console.log('---------------------------------');
+        var left   = veryFirstIndex - 1;
+        var right  = veryFirstIndex;
+        var i      = veryFirstIndex;
+        var set;
+        var slices = [];
+        var state  = 'normal';
+        rows.each(function (index, element) {
+
+            /* Детерминированный конечный автомат */
+            switch (state) {
+                case 'normal':
+                    if ($(element).hasClass('success')) {
+                        right = i - 1;
+                        set   = rows.slice(left, right);
+                        slices.push(set);
+                        console.log(set);
+                        left  = i;
+                        state = 'success';
+                    }
+                    break;
+                case 'success':
+                    if (!$(element).hasClass('success')) {
+                        right = i - 1;
+                        set   = rows.slice(left, right);
+                        slices.push(set);
+                        console.log(set);
+                        left  = right;
+                        state = 'normal';
+                    }
+                    break;
+            }
+            i += 1;
+        });
+        console.log('---------------------------------');
+        console.log(slices);
+    });
+
+    $('#btn-2').on('click', function () {
+        window.location.reload();
+    });
+
+    tbodies.on('mousedown', 'tr', function (e) {
+        var me      = $(this),
+            success = $('tr.success'),
+            current = success.data('row-id'),
+            target  = me.data('row-id'),
+            first   = current,
+            last    = target,
+            delta;
+        if (me.hasClass('total')) {
+            return false;
+        }
+        if (!e.ctrlKey && !e.shiftKey) {
+            success.removeClass('success');
+        }
+        if (e.shiftKey) {
+            e.preventDefault();
+            if (current > target) {
+                first = target;
+                last  = current;
+            }
+            delta = last - first;
+            for (var i = 0; i <= delta; i++) {
+                $('[data-row-id="' + (first + i) + '"]').addClass('success')
+            }
+        } else {
+            if (me.hasClass('success')) {
+                me.removeClass('success');
+            } else {
+                me.addClass('success');
+            }
+        }
     });
 });
