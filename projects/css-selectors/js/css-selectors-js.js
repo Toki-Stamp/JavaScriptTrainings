43adbf8247517818deb891a30f3231304c131896
fomichev <fomichev@nca.by>
2017-12-21 01:19:09 +0300

    object model

--- projects/css-selectors/js/css-selectors-js.js
+++ projects/css-selectors/js/css-selectors-js.js
@@ -5,51 +5,60 @@
 
 /* jQuery */
 jQuery(document).ready(function main() {
-    var matrix      = {
+    var matrix   = {
         'level-0': {
-            'parents'    : false,
-            'children'   : ['level-1', 'level-2', 'level-3', 'level-4'],
+            'parents':     false,
+            'children':    ['level-1', 'level-2', 'level-3', 'level-4'],
             'insert-rule': false
         },
         'level-1': {
-            'parents'    : ['level-0'],
-            'children'   : ['level-2', 'level-3', 'level-4'],
+            'parents':     ['level-0'],
+            'children':    ['level-2', 'level-3', 'level-4'],
             'insert-rule': ['level-0']
         },
         'level-2': {
-            'parents'    : ['level-1'],
-            'children'   : ['level-3', 'level-4'],
+            'parents':     ['level-1'],
+            'children':    ['level-3', 'level-4'],
             'insert-rule': ['level-0', 'level-1']
         },
         'level-3': {
-            'parents'    : ['level-2', 'level-1'],
-            'children'   : ['level-4'],
+            'parents':     ['level-2', 'level-1'],
+            'children':    ['level-4'],
             'insert-rule': ['level-0', 'level-2']
         },
         'level-4': {
-            'parents'    : ['level-3', 'level-2', 'level-1'],
-            'children'   : false,
+            'parents':     ['level-3', 'level-2', 'level-1'],
+            'children':    false,
             'insert-rule': ['level-0', 'level-2', 'level-3']
         }
     };
-    var getClasses  = function (element) {
-        return element.className.trim().split(/\s+/);
-    };
-    var siftClasses = function (classes) {
-        var item,
-            i,
-            length = classes.length;
-        if ($.isArray(classes)) {
+    var tbody    = document.getElementsByTagName('tbody')[0];
+    var rows     = tbody.getElementsByTagName('tr');
+    var length   = rows.length, i;
+    var element, index, level,
+        getLevel = function (el) {
+            var target = 'level',
+                list   = el.classList,
+                item,
+                length = list.length, i,
+                level;
             for (i = 0; i < length; i += 1) {
-                item = classes[i];
-                if (Object.keys(matrix).indexOf(item) >= 0) {
-                    return item;
+                item = list.item(i);
+                if (item.indexOf(target) >= 0) {
+                    level = list.item(i);
+                    break;
                 }
             }
-        }
-    };
-    var rows        = $('tbody > tr, thead > tr'), length = rows.length, i;
+            return level;
+        };
+    var classes  = {}, classesArray;
     for (i = 0; i < length; i += 1) {
-        console.log('Элемент-' + i, ':', rows.eq(i).get(0));
+        element        = rows[i];
+        level          = getLevel(element);
+        index          = element.rowIndex;
+        classes[level] = index;
+        console.log(index, ':', level);
     }
+    classesArray = Object.keys(classes);
+
 });
