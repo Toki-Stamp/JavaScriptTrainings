7f75c9aeb2a1ea2f5e4110138fb42776719951fe
Yuri Fomichev <fomichev@nca.by>
2018-01-05 09:56:04 +0300

    object model - almost here! analyze this!

--- projects/css-selectors/js/css-selectors-js-3.js
+++ projects/css-selectors/js/css-selectors-js-3.js
@@ -19,6 +19,7 @@ jQuery(document).ready(function main() {
         blink, clearBlink = function () {
             clearInterval(blink);
         },
+        message,
         selectedClass     = 'selected',
         borderClass       = 'border',
         validClass        = 'valid-place',
@@ -26,15 +27,16 @@ jQuery(document).ready(function main() {
 
     table.tooltip({
         disabled: true,
-        content : 'Пожалуйста, укажите место доступное для вставки',
-        items   : 'table',
+        content : 'Укажите место для вставки',
+        // items   : 'table tbody',
+        items   : 'body',
         track   : true
     });
 
     dialog.modal({
         backdrop: 'static',
         keyboard: true,
-        show    : true
+        show    : false
     });
 
     /* Обработчик строк таблицы */
@@ -54,6 +56,7 @@ jQuery(document).ready(function main() {
 
         if (table.attr('move-mode') === 'true') {
             dialog.modal('show');
+            dialog.find('div.modal-body').html('<p>' + message + '</p>');
             return false;
         }
 
@@ -88,52 +91,6 @@ jQuery(document).ready(function main() {
         var selected    = rows.filter('.' + selectedClass),
             expanded,
             levels      = [],
-            highlight   = function (model) {
-                var getValid = function (level) {
-                    var valid  = rows.first();
-                    var matrix = {1: null, 2: 1, 3: 2, 4: [2, 3]},
-                        levels = [],
-                        expand;
-
-                    if (level in matrix) {
-                        expand = matrix[level];
-                        if (expand) {
-                            if ($.isArray(expand)) {
-                                expand.forEach(function (item) {
-                                    levels.push(item);
-                                });
-                            } else {
-                                levels.push(expand);
-                            }
-                        }
-                    }
-
-                    levels.forEach(function (item) {
-                        if (typeof item === 'number') {
-                            var elements = rows.filter(
-                                '[data-level="' + item + '"]'
-                            );
-                        }
-                        valid = valid.add(elements);
-                    });
-
-                    return valid;
-                };
-                if ($.isArray(model)) {
-                    model.forEach(function (item) {
-                        var me       = item['0-me'],
-                            setBlink = function (elements) {
-                                if (elements.length) {
-                                    elements.addClass(borderClass);
-                                    blink = setInterval(function () {
-                                        elements.toggleClass(borderClass);
-                                    }, 750);
-                                }
-                            };
-                        console.log(item);
-                    });
-                }
-            },
             getId       = function (element) {
                 var result;
 
@@ -151,15 +108,6 @@ jQuery(document).ready(function main() {
                         '[data-parent="' + getId(element) + '"]'
                     );
                     result = (result.length) ? result : null;
-                }
-
-                return result;
-            },
-            getParent   = function (parentId) {
-                var result;
-
-                if (typeof parentId === 'number') {
-                    result = rows.filter('[id="' + parentId + '"]');
                 }
 
                 return result;
@@ -234,30 +182,6 @@ jQuery(document).ready(function main() {
                 });
 
                 return output;
-            },
-            getModel    = function (elements) {
-                var items = [];
-
-                elements.forEach(function (element) {
-                    var item     = {},
-                        id       = getId(element),
-                        children = getChildren(element),
-                        parent   = getParent(element.data('parent')),
-                        level    = element.data('level');
-
-                    item['0-me']       = element;
-                    item['1-parent']   = parent;
-                    item['2-children'] = children;
-                    item['3-id']       = id;
-                    item['4-name']     = element.data('name');
-                    item['5-number']   = element.data('number');
-                    item['6-level']    = level;
-                    item['7-tree']     = element.data('tree');
-
-                    items.push(item);
-                });
-
-                return items;
             };
 
         table.attr('move-mode', true);
@@ -265,21 +189,15 @@ jQuery(document).ready(function main() {
         table.tooltip('enable');
 
         var groups = getGroups(selected);
-        console.log(groups);
-        var message = 'Элементы одной группы или различных групп, но одного уровня - вставка согласно матрицы вставки';
-        var matrix  = {1: null, 2: [1], 3: [2], 4: [2, 3]};
-        var insert  = matrix[levels[0]];
+
+        var matrix = {1: null, 2: [1], 3: [2], 4: [2, 3]},
+            insert = matrix[levels[0]];
         if (levels.length > 1) {
+            insert = null;
             if (JSON.stringify(levels) === '[4,3]') {
-                message = 'Элементы разных групп уровней 3 и 4. Вставка во все 0 и 2';
-                insert  = matrix[3];
-            } else {
-                message = 'Элементы разных групп и/или уровней. Вставка только в 0';
-                insert  = null;
+                insert = matrix[3];
             }
         }
-        console.log(message);
-        console.log('levels', levels, 'insert', insert);
         if ($.isArray(groups) && groups.length) {
             var valid = rows.first(),
                 invalid;
@@ -293,7 +211,6 @@ jQuery(document).ready(function main() {
             });
 
             invalid = rows.not(valid).not(expanded);
-
             valid.addClass(validClass);
             invalid.addClass(invalidClass);
         }
