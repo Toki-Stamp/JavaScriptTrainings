c689dde688461ee30a36e740dd810ff45b132d89
Yuri Fomichev <fomichev@nca.by>
2018-01-09 17:26:27 +0300

    object model - almost here! analyze this!

--- projects/css-selectors/js/css-selectors-js-3.js
+++ projects/css-selectors/js/css-selectors-js-3.js
@@ -11,14 +11,139 @@ jQuery(document).ready(function main() {
     var table         = $('table'),
         tbody         = $('#tbodyForMainTable'),
         rows          = tbody.find('tr'),
+        selected,
         groups,
-        destination,
+        sourceIds     = [],
+        destinationId,
+        mode, title,
         moveBtn       = $('#move-btn'),
+        copyBtn       = $('#copy-btn'),
         okBtn         = $('#ok-btn'),
         cancelBtn     = $('#cancel-btn'),
         dialog        = $('#move-copy-dialog'),
         blink,
-        message,
+        makeMagic     = function (selected) {
+            var expanded,
+                levels      = [],
+                insert      = matrix[levels[0]],
+                valid       = rows.first(),
+                invalid,
+                getId       = function (element) {
+                    var result;
+
+                    if (element instanceof jQuery) {
+                        result = parseInt(element.attr('id'), 10);
+                    }
+
+                    return result;
+                },
+                getChildren = function (element) {
+                    var result;
+
+                    if (element instanceof jQuery) {
+                        result = rows.filter(
+                            '[data-parent="' + getId(element) + '"]'
+                        );
+                        result = (result.length) ? result : null;
+                    }
+
+                    return result;
+                },
+                getGroups   = function (elements) {
+                    var groups    = [],
+                        result,
+                        expand    = function (elements) {
+                            var group = $();
+
+                            elements.each(function () {
+                                var me       = $(this),
+                                    children = getChildren(me);
+
+                                group = group.add(me);
+
+                                if (children) {
+                                    group = group.add(expand(children))
+                                }
+                            });
+
+                            group.addClass(selectedClass).addClass(borderClass);
+
+                            return group;
+                        },
+                        consume   = function (array) {
+                            var result = array.slice(0),
+                                i      = 0, j,
+                                left, right;
+                            if ($.isArray(result)) {
+                                /* outer */
+                                while (i < (result.length - 1)) {
+                                    left = result[i];
+                                    /* inner */
+                                    for (j = (i + 1); j < result.length; j++) {
+                                        right = result[j];
+                                        if (left.is(right)) {
+                                            result.splice(j, 1);
+                                            i = -1;
+                                            break;
+                                        }
+                                    }
+                                    i += 1;
+                                }
+                            }
+                            return result;
+                        },
+                        getLevels = function (array) {
+                            var levels = [];
+                            array.forEach(function (item) {
+                                var level = item.data('level');
+                                if (levels.indexOf(level) === -1) {
+                                    levels.push(level)
+                                }
+                            });
+                            return levels;
+                        };
+
+                    if ((elements instanceof jQuery) && (elements.length)) {
+                        elements.each(function () {
+                            groups.push(expand($(this)));
+                        });
+                    }
+
+                    expanded = rows.filter('.' + selectedClass);
+                    blink    = setInterval(function () {
+                        expanded.toggleClass(borderClass);
+                    }, 750);
+                    result   = consume(groups);
+                    levels   = getLevels(result).sort(function (a, b) {
+                        return b - a;
+                    });
+
+                    return result;
+                };
+
+            groups = getGroups(selected);
+
+            if (levels.length > 1) {
+                insert = null;
+                if (JSON.stringify(levels) === '[4,3]') {
+                    insert = matrix[3];
+                }
+            }
+            if ($.isArray(groups) && groups.length) {
+                groups.forEach(function () {
+                    if (insert) {
+                        insert.forEach(function (level) {
+                            valid = valid.add(rows.filter('[data-level="' + level + '"]'));
+                        });
+                    }
+                });
+
+                invalid = rows.not(valid).not(expanded);
+                valid.addClass(validClass);
+                invalid.addClass(invalidClass);
+            }
+        },
+        matrix        = {1: null, 2: [1], 3: [2], 4: [2, 3]},
         selectedClass = 'selected',
         borderClass   = 'border',
         validClass    = 'valid-place',
@@ -40,7 +165,6 @@ jQuery(document).ready(function main() {
         table.tooltip('enable');
     });
 
-    /* Обработчик строк таблицы */
     $(document).on('mousedown', 'table tbody > tr', function (e) {
         var me          = $(this),
             selected    = rows.filter('.' + selectedClass),
@@ -49,31 +173,31 @@ jQuery(document).ready(function main() {
             first       = current,
             last        = target,
             level       = me.data('level'),
+            mode        = table.attr('move-copy-mode'),
             getInfo     = function (element, flag) {
-                var str = '';
-                if (element instanceof jQuery) {
-                    var level = element.data('level'),
-                        tree  = element.data('tree').substring(1).split('/'),
-                        i, item;
+                var str   = '',
+                    level = element.data('level'),
+                    tree  = element.data('tree').substring(1).split('/'),
+                    i, item;
 
-                    str += '<p>';
-                    str += '(Ур:' + level + ') ';
-                    str += '<span class="root">';
-                    for (i = 0; i < (level - 1); i++) {
-                        item = rows.filter('[id="' + tree[i] + '"]');
-                        str += item.data('name') + ' ' + item.data('number');
-                        str += ' / ';
-                    }
-                    item = rows.filter('[id="' + tree[level - 1] + '"]');
-                    str += '</span> ';
-                    str += '<span class="branch">';
+                str += '<p>';
+                str += '(Ур:' + level + ') ';
+                str += '<span class="root">';
+                for (i = 0; i < (level - 1); i++) {
+                    item = rows.filter('[id="' + tree[i] + '"]');
                     str += item.data('name') + ' ' + item.data('number');
-                    if (!flag && (level < 4)) {
-                        str += ' и вложенные элементы';
-                    }
-                    str += '</span>';
-                    str += '</p>';
+                    str += ' / ';
                 }
+                item = rows.filter('[id="' + tree[level - 1] + '"]');
+                str += '</span> ';
+                str += '<span class="branch">';
+                str += item.data('name') + ' ' + item.data('number');
+                if (!flag && (level < 4)) {
+                    str += ' и вложенные элементы';
+                }
+                str += '</span>';
+                str += '</p>';
+
                 return str;
             },
             description = function (groups) {
@@ -81,32 +205,33 @@ jQuery(document).ready(function main() {
 
                 if ($.isArray(groups)) {
                     groups.forEach(function (group) {
-                        result += getInfo(group.first());
+                        var element = group.first();
+                        if (element instanceof jQuery) {
+                            sourceIds.push(element.attr('id'));
+                            result += getInfo(element);
+                        }
                     });
                 }
                 return result;
-            };
+            },
+            elementsList,
+            targetPlace;
 
-        if (table.attr('copy-move-mode') === 'move') {
-            table.tooltip('disable');
-
+        if (mode) {
             if (!me.hasClass(validClass)) {
                 return false;
             }
-
-            destination = me;
-
-            var elementsList = description(groups),
-                targetPlace  = level ? getInfo(me, true) :
-                    '<p>(Ур:0) <span class="branch">Корень таблицы</span></p>';
-
-            console.log(elementsList);
-            console.log(targetPlace);
+            table.tooltip('disable');
+            elementsList  = description(groups);
+            destinationId = me.attr('id');
+            targetPlace   = level ? getInfo(me, true) :
+                '<p>(Ур:0) <span class="branch">Корень таблицы</span></p>';
+            title         = (mode === 'move') ? 'Перемещение элементов экспликации' :
+                'Копирование элементов экспликации';
 
             dialog.find('#elements-list').html(elementsList);
             dialog.find('#target-place').html(targetPlace);
-            dialog.find('.modal-title').text('Перемещение элементов экспликации');
-
+            dialog.find('.modal-title').text(title);
             dialog.modal('show');
         } else {
             if (!level) {
@@ -137,151 +262,42 @@ jQuery(document).ready(function main() {
                 }
             }
         }
-    });
-    $(document).on('mouseup', 'table tbody > tr', function () {
-        if (table.attr('copy-move-mode') !== 'true') {
-            if ((rows.filter('.' + selectedClass)).length) {
-                moveBtn.prop('disabled', false);
-            }
+    }).on('mouseup', 'table tbody > tr', function () {
+        mode = table.attr('move-copy-mode');
+        if ((rows.filter('.' + selectedClass)).length) {
+            moveBtn.prop('disabled', false);
+            copyBtn.prop('disabled', false);
         }
+        console.log('mode', mode);
     });
 
     moveBtn.on('click', function () {
-        var selected    = rows.filter('.' + selectedClass),
-            expanded,
-            levels      = [],
-            getId       = function (element) {
-                var result;
 
-                if (element instanceof jQuery) {
-                    result = parseInt(element.attr('id'), 10);
-                }
-
-                return result;
-            },
-            getChildren = function (element) {
-                var result;
-
-                if (element instanceof jQuery) {
-                    result = rows.filter(
-                        '[data-parent="' + getId(element) + '"]'
-                    );
-                    result = (result.length) ? result : null;
-                }
-
-                return result;
-            },
-            getGroups   = function (elements) {
-                var groups    = [],
-                    output,
-                    expand    = function (elements) {
-                        var group = $();
-
-                        elements.each(function () {
-                            var me       = $(this),
-                                children = getChildren(me);
-
-                            group = group.add(me);
-
-                            if (children) {
-                                group = group.add(expand(children))
-                            }
-                        });
-
-                        group.addClass(selectedClass).addClass(borderClass);
-
-                        return group;
-                    },
-                    consume   = function (array) {
-                        var result = array.slice(0),
-                            i      = 0, j,
-                            left, right;
-                        if ($.isArray(result)) {
-                            /* outer */
-                            while (i < (result.length - 1)) {
-                                left = result[i];
-                                /* inner */
-                                for (j = (i + 1); j < result.length; j++) {
-                                    right = result[j];
-                                    if (left.is(right)) {
-                                        result.splice(j, 1);
-                                        i = -1;
-                                        break;
-                                    }
-                                }
-                                i += 1;
-                            }
-                        }
-                        return result;
-                    },
-                    getLevels = function (array) {
-                        var levels = [];
-                        array.forEach(function (item) {
-                            var level = item.data('level');
-                            if (levels.indexOf(level) === -1) {
-                                levels.push(level)
-                            }
-                        });
-                        return levels;
-                    };
-
-                if ((elements instanceof jQuery) && (elements.length)) {
-                    elements.each(function () {
-                        groups.push(expand($(this)));
-                    });
-                }
-
-                expanded = rows.filter('.' + selectedClass);
-                blink    = setInterval(function () {
-                    expanded.toggleClass(borderClass);
-                }, 750);
-                output   = consume(groups);
-                levels   = getLevels(output).sort(function (a, b) {
-                    return b - a;
-                });
-
-                return output;
-            };
-
-        table.attr('copy-move-mode', 'move');
+        table.attr('move-copy-mode', 'move');
         moveBtn.prop('disabled', true);
+        copyBtn.prop('disabled', true);
         table.tooltip('enable');
 
-        groups = getGroups(selected);
+        selected = rows.filter('.' + selectedClass);
+        makeMagic(selected);
+    });
 
-        var matrix = {1: null, 2: [1], 3: [2], 4: [2, 3]},
-            insert = matrix[levels[0]];
+    copyBtn.on('click', function () {
 
-        message = 'выделены элементы одной группы либо различных групп одинакового уровня. Вставка согласно матрицы вставки';
-        if (levels.length > 1) {
-            insert  = null;
-            message = 'выделены элементы разных групп и / или уровней. Вставка только в корень таблицы';
-            if (JSON.stringify(levels) === '[4,3]') {
-                insert  = matrix[3];
-                message = 'выделены элементы разных групп с уровнями 3 и 4. Вставка во все уровни 2 и в корень таблицы';
-            }
-        }
-        if ($.isArray(groups) && groups.length) {
-            var valid = rows.first(),
-                invalid;
+        table.attr('move-copy-mode', 'copy');
+        moveBtn.prop('disabled', true);
+        copyBtn.prop('disabled', true);
+        table.tooltip('enable');
 
-            groups.forEach(function () {
-                if (insert) {
-                    insert.forEach(function (level) {
-                        valid = valid.add(rows.filter('[data-level="' + level + '"]'));
-                    });
-                }
-            });
-
-            invalid = rows.not(valid).not(expanded);
-            valid.addClass(validClass);
-            invalid.addClass(invalidClass);
-        }
+        selected = rows.filter('.' + selectedClass);
+        makeMagic(selected);
     });
 
     okBtn.on('click', function () {
-        var mode = table.attr('copy-move-mode');
+        mode = table.attr('move-copy-mode');
         console.log('Операция:', mode);
+        console.log('Элементы экспликации:', sourceIds);
+        console.log('Назначение:', destinationId);
     });
 
     cancelBtn.on('mousedown', function () {
@@ -292,9 +308,9 @@ jQuery(document).ready(function main() {
             invalidClass
         );
         table.tooltip('disable');
-        table.attr('copy-move-mode', false);
+        table.removeAttr('move-copy-mode');
         moveBtn.prop('disabled', true);
+        copyBtn.prop('disabled', true);
         clearInterval(blink);
     });
-
 });
