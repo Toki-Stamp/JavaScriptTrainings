5cf912087c6d85abfc0a3fcbd7fc04977a4589ff
fomichev <fomichev@nca.by>
2018-04-05 00:54:09 +0300

    new token badges

--- projects/select2/js/select2example-script.js
+++ projects/select2/js/select2example-script.js
@@ -5,6 +5,39 @@
 
 /* jQuery */
 jQuery(document).ready(function main() {
+    var data = [
+            {
+                name: 'Общая площадь здания',
+                char: '315',
+                value: [443.6]
+            },
+            {
+                name: 'Общая площадь жилых помещений (квартир) жилого дома',
+                char: '334',
+                value: [153.3]
+            }
+        ],
+        table = $('#main-table'),
+        tbody = table.find('tbody');
+
+    data.forEach(function (object) {
+        var element = '';
+
+        element += '<tr id="data-char-' + object.char + '-row">';
+        element += '<td>';
+        element += object.name;
+        element += '</td>';
+        element += '<td>';
+        element += '<div class="row">';
+        element += '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">';
+        element += '<input type="text" class="form-control" title="' + object.name + '" value="' + object.value + '" readonly/>';
+        element += '</div>';
+        element += '</div>';
+        element += '</td>';
+        element += '</tr>';
+
+        tbody.append(element);
+    });
 });
 
 /* Self-invoking function */
