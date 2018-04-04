5ba74e1d2214a4592000e7a60ac0ab6bb781fd47
Yuri Fomichev <fomichev@nca.by>
2018-04-04 20:06:31 +0300

    object model done!

---
+++ projects/select2/js/select2example-script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Fomichev Yuri on 19.10.2017
+ * Contact me at : toki.stamp@gmail.com
+ */
+
+/* jQuery */
+jQuery(document).ready(function main() {
+});
+
+/* Self-invoking function */
+(function main() {
+})();
