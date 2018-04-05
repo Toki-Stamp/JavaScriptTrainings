7b0a03d3f21d1b8deaa7057bd4fef393c9b35bca
Yuri Fomichev <fomichev@nca.by>
2018-04-05 09:22:02 +0300

    General about CS

--- projects/select2/js/select2example-script.js
+++ projects/select2/js/select2example-script.js
@@ -5,20 +5,174 @@
 
 /* jQuery */
 jQuery(document).ready(function main() {
-    var data = [
+    var object = {
+            "address"              : {
+                "actual"        : null,
+                "adrStatus"     : null,
+                "adrstr"        : null,
+                "category"      : null,
+                "elementtype"   : null,
+                "id"            : 70110913,
+                "idAdr"         : null,
+                "idEva"         : null,
+                "idInAdr"       : null,
+                "idIndex"       : null,
+                "ididadr"       : null,
+                "indHouse"      : null,
+                "indRoom"       : null,
+                "km"            : null,
+                "kodSpec"       : null,
+                "numCorp"       : null,
+                "numHouse"      : null,
+                "numRoom"       : null,
+                "objectnumber"  : null,
+                "propType"      : null,
+                "remark"        : null,
+                "statusSpecific": null,
+                "xcoord"        : null,
+                "ycoord"        : null
+            },
+            "block"                : null,
+            "buid"                 : null,
+            "char_161"             : null,
+            "fuid"                 : null,
+            "id"                   : 40113634,
+            "inserted"             : "2018-04-03T15:51:26.000Z",
+            "inserted_by"          : 2341,
+            "inventory_number"     : null,
+            "isAutoCalc"           : 1,
+            "land_category_id"     : null,
+            "nazn"                 : {"auditOut": -1, "code": 99999, "code2": "99999", "immovablePurposeName": "Нет сведений", "objectType": null},
+            "object_name"          : "ЧТЗ 17.1 п.2.3 ИП",
+            "object_status_type_id": null,
+            "object_type_id"       : {
+                "addCode"          : null,
+                "addCodeElem"      : null,
+                "apartment"        : 0,
+                "auditin"          : null,
+                "auditout"         : -1,
+                "immovablePurpose" : "40214, 49999, 99999, 99998",
+                "immovableType"    : "8",
+                "improvement"      : 0,
+                "inventoryForm"    : "020106, 020201, 020205",
+                "literacy"         : 1,
+                "mainCode"         : "167, 5, 6, 7, 9,  252, 161, 301, 302",
+                "mainCodeElem"     : 3000000,
+                "nonresidential"   : 0,
+                "objectName"       : "Индивидуальный изолированный гараж",
+                "objectStatus"     : [{
+                    "beginDate" : "2016-04-01T08:07:47.000Z",
+                    "endDate"   : null,
+                    "id"        : 7,
+                    "insertedBy": 177,
+                    "objectId"  : {
+                        "address"              : {
+                            "actual"        : -1,
+                            "adrStatus"     : null,
+                            "adrstr"        : "225407, Республика Беларусь, Брестская обл., г. Барановичи, ул. Вильчковского, 126Б.",
+                            "category"      : null,
+                            "elementtype"   : null,
+                            "id"            : 7,
+                            "idAdr"         : null,
+                            "idEva"         : null,
+                            "idInAdr"       : null,
+                            "idIndex"       : null,
+                            "ididadr"       : null,
+                            "indHouse"      : null,
+                            "indRoom"       : null,
+                            "km"            : null,
+                            "kodSpec"       : null,
+                            "numCorp"       : null,
+                            "numHouse"      : null,
+                            "numRoom"       : null,
+                            "objectnumber"  : null,
+                            "propType"      : null,
+                            "remark"        : null,
+                            "statusSpecific": null,
+                            "xcoord"        : null,
+                            "ycoord"        : null
+                        },
+                        "block"                : null,
+                        "buid"                 : null,
+                        "char_161"             : null,
+                        "fuid"                 : null,
+                        "id"                   : 9,
+                        "inserted"             : "2016-04-01T08:07:47.000Z",
+                        "inserted_by"          : 177,
+                        "inventory_number"     : 3309,
+                        "isAutoCalc"           : 1,
+                        "land_category_id"     : null,
+                        "nazn"                 : {"auditOut": -1, "code": 21103, "code2": "21103", "immovablePurposeName": "Здание одноквартирного жилого дома", "objectType": 6},
+                        "object_name"          : "Тест_жилой дом",
+                        "object_status_type_id": null,
+                        "object_type_id"       : {
+                            "addCode"          : "10",
+                            "addCodeElem"      : 2000000,
+                            "apartment"        : 1,
+                            "auditin"          : null,
+                            "auditout"         : -1,
+                            "immovablePurpose" : "21102, 20103, 29999, 99999, 21103, 99998, 21104",
+                            "immovableType"    : "2",
+                            "improvement"      : 1,
+                            "inventoryForm"    : "020102, 020201, 020202, 020203.020204, 020301, 020302",
+                            "literacy"         : 1,
+                            "mainCode"         : "151, 1, 2, 3, 4, 301, 252, 302, 5, 314, 7, 315, 14, 46, 320, 324, 330, 19, 20, 11, 12, 48, 162, 18, 22, 45, 10, 72, 282",
+                            "mainCodeElem"     : 1000000,
+                            "nonresidential"   : 1,
+                            "objectName"       : "Одноквартирный, блокированный жилой дом",
+                            "objectStatus"     : [],
+                            "objectTypeId"     : 2,
+                            "pageBuild"        : 1,
+                            "parcel"           : 1,
+                            "residential"      : 1,
+                            "technicalPassport": 10102
+                        },
+                        "object_vyd_id"        : {"auditOut": -1, "code": 2, "immovableTypeName": "Капитальное строение", "literal": "C"},
+                        "old_id"               : null,
+                        "operation_status_code": null,
+                        "org_id"               : 110,
+                        "org_uid"              : 110,
+                        "parcel_number"        : null,
+                        "parcel_uid"           : null,
+                        "soato"                : null,
+                        "square"               : null,
+                        "updated"              : "2017-03-28T06:06:56.000Z",
+                        "updated_by"           : 177
+                    },
+                    "statusId"  : {"abbreviation": "принят в эксп.", "auditin": null, "auditout": -1, "code1": 301, "objectStatusname": "принят в эксплуатацию", "statusType": 1}
+                }],
+                "objectTypeId"     : 9,
+                "pageBuild"        : 1,
+                "parcel"           : 0,
+                "residential"      : 0,
+                "technicalPassport": 10106
+            },
+            "object_vyd_id"        : {"auditOut": -1, "code": 8, "immovableTypeName": "Изолированное помещение", "literal": "D"},
+            "old_id"               : null,
+            "operation_status_code": null,
+            "org_id"               : 0,
+            "org_uid"              : 0,
+            "parcel_number"        : null,
+            "parcel_uid"           : null,
+            "soato"                : null,
+            "square"               : null,
+            "updated"              : "2018-04-03T15:51:45.000Z",
+            "updated_by"           : 2341
+        },
+        data   = [
             {
-                name: 'Общая площадь здания',
-                char: '315',
+                name : 'Общая площадь здания',
+                char : '315',
                 value: [443.6]
             },
             {
-                name: 'Общая площадь жилых помещений (квартир) жилого дома',
-                char: '334',
+                name : 'Общая площадь жилых помещений (квартир) жилого дома',
+                char : '334',
                 value: [153.3]
             }
         ],
-        table = $('#main-table'),
-        tbody = table.find('tbody');
+        table  = $('#main-table'),
+        tbody  = table.find('tbody');
 
     data.forEach(function (object) {
         var element = '';
@@ -40,6 +194,11 @@ jQuery(document).ready(function main() {
     });
 });
 
-/* Self-invoking function */
-(function main() {
-})();
+function form_action() {
+    var regex = new RegExp('^[1-9]{1}[0-9]{0,11}$'),
+        tests = ['1', '0', '07', '', '-2', '11', '123456789012345', '1234567890123456', '12345678901234567'];
+
+    tests.forEach(function (value) {
+        console.log(value, '==>', regex.test(value));
+    });
+};
