840f6d04c6b8dbb769016f349de4364f58f94519
fomichev <fomichev@nca.by>
2018-04-09 01:01:15 +0300

    text area component

---
+++ projects/new-components/store/store.js
@@ -0,0 +1,284 @@
+var data = [
+    {
+        name: 'Общая площадь здания',
+        char: '315',
+        value: [443.6]
+    },
+    {
+        name: 'Общая площадь жилых помещений (квартир) жилого дома',
+        char: '334',
+        value: [153.3]
+    }
+];
+
+var object = {
+    "address": {
+        "actual": null,
+        "adrStatus": null,
+        "adrstr": null,
+        "category": null,
+        "elementtype": null,
+        "id": 70110913,
+        "idAdr": null,
+        "idEva": null,
+        "idInAdr": null,
+        "idIndex": null,
+        "ididadr": null,
+        "indHouse": null,
+        "indRoom": null,
+        "km": null,
+        "kodSpec": null,
+        "numCorp": null,
+        "numHouse": null,
+        "numRoom": null,
+        "objectnumber": null,
+        "propType": null,
+        "remark": null,
+        "statusSpecific": null,
+        "xcoord": null,
+        "ycoord": null
+    },
+    "block": null,
+    "buid": null,
+    "char_161": null,
+    "fuid": null,
+    "id": 40113634,
+    "inserted": "2018-04-03T15:51:26.000Z",
+    "inserted_by": 2341,
+    "inventory_number": null,
+    "isAutoCalc": 1,
+    "land_category_id": null,
+    "nazn": {
+        "auditOut": -1,
+        "code": 99999,
+        "code2": "99999",
+        "immovablePurposeName": "Нет сведений",
+        "objectType": null
+    },
+    "object_name": "ЧТЗ 17.1 п.2.3 ИП",
+    "object_status_type_id": null,
+    "object_type_id": {
+        "addCode": null,
+        "addCodeElem": null,
+        "apartment": 0,
+        "auditin": null,
+        "auditout": -1,
+        "immovablePurpose": "40214, 49999, 99999, 99998",
+        "immovableType": "8",
+        "improvement": 0,
+        "inventoryForm": "020106, 020201, 020205",
+        "literacy": 1,
+        "mainCode": "167, 5, 6, 7, 9,  252, 161, 301, 302",
+        "mainCodeElem": 3000000,
+        "nonresidential": 0,
+        "objectName": "Индивидуальный изолированный гараж",
+        "objectStatus": [{
+            "beginDate": "2016-04-01T08:07:47.000Z",
+            "endDate": null,
+            "id": 7,
+            "insertedBy": 177,
+            "objectId": {
+                "address": {
+                    "actual": -1,
+                    "adrStatus": null,
+                    "adrstr": "225407, Республика Беларусь, Брестская обл., г. Барановичи, ул. Вильчковского, 126Б.",
+                    "category": null,
+                    "elementtype": null,
+                    "id": 7,
+                    "idAdr": null,
+                    "idEva": null,
+                    "idInAdr": null,
+                    "idIndex": null,
+                    "ididadr": null,
+                    "indHouse": null,
+                    "indRoom": null,
+                    "km": null,
+                    "kodSpec": null,
+                    "numCorp": null,
+                    "numHouse": null,
+                    "numRoom": null,
+                    "objectnumber": null,
+                    "propType": null,
+                    "remark": null,
+                    "statusSpecific": null,
+                    "xcoord": null,
+                    "ycoord": null
+                },
+                "block": null,
+                "buid": null,
+                "char_161": null,
+                "fuid": null,
+                "id": 9,
+                "inserted": "2016-04-01T08:07:47.000Z",
+                "inserted_by": 177,
+                "inventory_number": 3309,
+                "isAutoCalc": 1,
+                "land_category_id": null,
+                "nazn": {
+                    "auditOut": -1,
+                    "code": 21103,
+                    "code2": "21103",
+                    "immovablePurposeName": "Здание одноквартирного жилого дома",
+                    "objectType": 6
+                },
+                "object_name": "Тест_жилой дом",
+                "object_status_type_id": null,
+                "object_type_id": {
+                    "addCode": "10",
+                    "addCodeElem": 2000000,
+                    "apartment": 1,
+                    "auditin": null,
+                    "auditout": -1,
+                    "immovablePurpose": "21102, 20103, 29999, 99999, 21103, 99998, 21104",
+                    "immovableType": "2",
+                    "improvement": 1,
+                    "inventoryForm": "020102, 020201, 020202, 020203.020204, 020301, 020302",
+                    "literacy": 1,
+                    "mainCode": "151, 1, 2, 3, 4, 301, 252, 302, 5, 314, 7, 315, 14, 46, 320, 324, 330, 19, 20, 11, 12, 48, 162, 18, 22, 45, 10, 72, 282",
+                    "mainCodeElem": 1000000,
+                    "nonresidential": 1,
+                    "objectName": "Одноквартирный, блокированный жилой дом",
+                    "objectStatus": [],
+                    "objectTypeId": 2,
+                    "pageBuild": 1,
+                    "parcel": 1,
+                    "residential": 1,
+                    "technicalPassport": 10102
+                },
+                "object_vyd_id": {
+                    "auditOut": -1,
+                    "code": 2,
+                    "immovableTypeName": "Капитальное строение",
+                    "literal": "C"
+                },
+                "old_id": null,
+                "operation_status_code": null,
+                "org_id": 110,
+                "org_uid": 110,
+                "parcel_number": null,
+                "parcel_uid": null,
+                "soato": null,
+                "square": null,
+                "updated": "2017-03-28T06:06:56.000Z",
+                "updated_by": 177
+            },
+            "statusId": {
+                "abbreviation": "принят в эксп.",
+                "auditin": null,
+                "auditout": -1,
+                "code1": 301,
+                "objectStatusname": "принят в эксплуатацию",
+                "statusType": 1
+            }
+        }],
+        "objectTypeId": 9,
+        "pageBuild": 1,
+        "parcel": 0,
+        "residential": 0,
+        "technicalPassport": 10106
+    },
+    "object_vyd_id": {"auditOut": -1, "code": 8, "immovableTypeName": "Изолированное помещение", "literal": "D"},
+    "old_id": null,
+    "operation_status_code": null,
+    "org_id": 0,
+    "org_uid": 0,
+    "parcel_number": null,
+    "parcel_uid": null,
+    "soato": null,
+    "square": null,
+    "updated": "2018-04-03T15:51:45.000Z",
+    "updated_by": 2341
+};
+
+var data = {
+    "XRh3Materials": [[2040200, "Кирпич силикатный", "кирп. с/к", "Состоит из смеси песка (около 90%), извести (около 10%), а также добавок. Применяют силикатный кирпич для кладки наружных и внутренних стен надземных частей зданий и сооружений.", null, -1]],
+    "adrstr": "247170, Республика Беларусь, Гомельская обл., Кормянский р-н, гп Корма, ул. Абатурова, 49",
+    "adrstrSign": "1",
+    "componentIdGeneral": "681670", // TODO Что это такое? Где это нужно использовать?
+    "floorAboveground": "5",
+    "floorUnderground": "-", // TODO Может быть лучше просто null как в yearReconstruction?
+    "inventoryNumber": "4499",
+    "literal": "C",
+    "objectId": "180610",
+    "objectName": "многоквартирный жилой дом",
+    "objectTypeId": "3",
+    "objectTypeName": "Здание",
+    "orgUid": "323",
+    "purposeName": "21101 - Здание многоквартирного жилого дома",
+    "shortName": "Гомельское агентство",
+    "wallMaterial": "2040200",
+    "xRh3YearBuilt": [[1981, "1981", null, -1, "1981", 485]],
+    "xRh3YearReconstruction": null,
+    "yearBuilt": "1981",
+    "yearReconstruction": null
+};
+
+var addTableRow = function (table) {
+    var element = '';
+
+    element += '<tr id="data-char-' + object.char + '-row">';
+    element += '<td>';
+    element += object.name;
+    element += '</td>';
+    element += '<td>';
+    element += '<div class="row">';
+    element += '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">';
+    element += '<input type="text" class="form-control" title="' + object.name + '" value="' + object.value + '" readonly/>';
+    element += '</div>';
+    element += '</div>';
+    element += '</td>';
+    element += '</tr>';
+
+    table.find('tbody').append(element);
+};
+
+// box
+//     .find('i')
+//     .attr('title', messages.before)
+//     .on('click', function () {
+//         var me = $(this),
+//             parent = me.parent();
+//
+//         if (!me.hasClass(classes.i_ok) && copyToClipboard(parent)) {
+//             me
+//                 .removeClass(classes.i_copy)
+//                 .addClass(classes.i_ok)
+//                 .attr('title', messages.after);
+//
+//             parent
+//                 .addClass(classes.box_copied)
+//                 .attr('title', messages.after);
+//         }
+//     });
+
+/* надо? */
+// generalInfoAboutCSService.findObjectParentsForObjects(
+//     general_info_about_cs.obj_id,
+//     false,
+//     {
+//         async: false,
+//         callback: function (list) {
+//             var neededObj = {};
+//
+//             for (var i = 0; i < list.length; i++) {
+//                 if (!list[i].objectParentId.parcel_number) {
+//                     neededObj = list[i];
+//                     break;
+//                 }
+//             }
+//             if ($.isEmptyObject(neededObj)) {
+//                 $("#formWait").hide();
+//                 alert('Ошибка при запросе родительского объекта!');
+//                 return false;
+//             }
+//             general_info_about_cs.parent_row_id = neededObj.id;
+//             general_info_about_cs.parent_id = neededObj.objectParentId.id;
+//             generalInfoAboutCSService.getComponentsWithCharacteristicsByObjectId(neededObj.objectParentId.id, 6, {
+//                 callback: function (componentList) {
+//                     general_info_about_cs.fillFields(neededObj.objectParentId, componentList[0]);
+//                     general_info_about_cs.getWallMaterial(neededObj.objectParentId);
+//                 }
+//             });
+//         }
+//     }
+// );
