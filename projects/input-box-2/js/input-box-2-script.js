0659931ad9b0462d9e4be68671e277d9f6ac2780
Yuri Fomichev <fomichev@nca.by>
2018-08-15 17:30:29 +0300

    15.08.2018

--- projects/input-box-2/js/input-box-2-script.js
+++ projects/input-box-2/js/input-box-2-script.js
@@ -5,10 +5,9 @@
 
 /* jQuery */
 jQuery(document).ready(function main() {
-    let closeBtns           = $('[data-role="remove-date"]'),
+    let remove              = $('[data-role="remove-date"]'),
         btns                = $('button'),
         input               = $('input'),
-        test                = $('#test'),
         datepickerContainer = $('.datepicker-container'),
         options             = {
             startDate             : new Date(1983, 4, 9),
@@ -27,59 +26,64 @@ jQuery(document).ready(function main() {
             }
         },
         datepicker          = $('[data-role="datepicker"]').datepicker(options).data('datepicker'),
-        dates               = $('.pick-date').find('span.text');
-    
-    closeBtns.on('click', function () {
+        picks               = $('.pick-date').find('span.text');
+
+    remove.on('click', function () {
         let parent      = $(this).parent(),
             grandParent = parent.parent(),
+            effect      = ['fade-out', 'collapse-out', 'zoom-in'],
+            _class,
             check       = function () {
                 let count = grandParent.find('.pick-date');
-            
+
                 if (!count.length) {
                     grandParent.empty();
                 }
             };
-        
-        parent.addClass('fade-out');
-        
+
+        if (grandParent.is('.1')) {
+            _class = effect[0];
+        } else if (grandParent.is('.2')) {
+            _class = effect[1];
+        } else if (grandParent.is('.3')) {
+            _class = effect[2];
+        }
+
+        parent.addClass(_class).blur();
+
         setTimeout(function () {
             parent.remove();
             check();
         }, 250);
     });
-    
-    test.hide();
-    
+
     // datepickerContainer.on('click', function () {
     //     datepicker.show();
     // });
-    
-    dates.each(function () {
+
+    picks.each(function () {
         let me = $(this);
-        
+
         me.on('click', function () {
             let parts = me.text().split('.'),
                 year  = parseInt(parts[2], 10),
                 month = parseInt(parts[1], 10),
                 day   = parseInt(parts[0], 10),
                 date  = new Date(year, month - 1, day);
-            
+
             datepicker.clear();
             datepicker.selectDate(date);
             datepicker.show();
         });
     });
-    
+
     btns.each(function () {
         let me = $(this),
             fn;
-        
+
         switch (me.data('role')) {
             case 'read-dates':
                 fn = function () {
-                    let popper = new Popper(me, test, {
-                        placement: 'top'
-                    });
                     console.log('input:', '"' + input.val() + '"');
                 };
                 break;
@@ -88,11 +92,7 @@ jQuery(document).ready(function main() {
                     datepicker.show();
                 }
         }
-        
+
         me.on('click', fn);
     });
 });
-
-/* Self-invoking function */
-(function main() {
-})();
