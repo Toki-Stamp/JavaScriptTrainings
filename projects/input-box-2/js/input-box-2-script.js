d18ce10a25f20b53bcd95b88f762b60a3d3c3365
fomichev <fomichev@nca.by>
2018-08-23 01:21:57 +0300

    input-box v7

--- projects/input-box-2/js/input-box-2-script.js
+++ projects/input-box-2/js/input-box-2-script.js
@@ -12,34 +12,25 @@ jQuery(document).ready(function main() {
         popup     = $('.x-datepicker-popup').css('display', 'none'),
         popper    = new Popper(reference, popup, {
             placement    : 'top-start',
-            positionFixed: true,
-            modifiers    : {
-                // flip  : {
-                //     behavior: ['top-start', 'bottom-start']
-                // },
-                // offset: {
-                //     enabled: true,
-                //     offset : '0, 4'
-                // }
-            },
+            eventsEnabled: false,
             onCreate     : function (data) {
                 console.log(data);
             }
         });
-
+    
     remove.on('click', function () {
         let parent      = $(this).parent(),
             grandParent = parent.parent(),
             effect      = ['fade-out', 'collapse-out', 'zoom-in'],
             _class,
             check       = function () {
-                let count = grandParent.find('.x-date');
-
+                let count = grandParent.find('.pick-date');
+            
                 if (!count.length) {
                     grandParent.empty();
                 }
             };
-
+        
         if (grandParent.is('.v1')) {
             _class = effect[0];
         } else if (grandParent.is('.v2')) {
@@ -47,19 +38,19 @@ jQuery(document).ready(function main() {
         } else if (grandParent.is('.v3')) {
             _class = effect[2];
         }
-
+        
         parent.addClass(_class).blur();
-
+        
         setTimeout(function () {
             parent.remove();
             check();
         }, 250);
     });
-
+    
     btns.each(function () {
         let me = $(this),
             fn;
-
+        
         switch (me.data('role')) {
             case 'read-dates':
                 fn = function () {
@@ -71,28 +62,40 @@ jQuery(document).ready(function main() {
                     datepicker.show();
                 }
         }
-
+        
         me.on('click', fn);
     });
     reference.on('click', function () {
-        let timeout;
-
+        let timeout,
+            delay    = 50,
+            style    = getComputedStyle(document.body),
+            duration = (function () {
+                let multipliers = {
+                    s : 100,
+                    ms: 1
+                };
+                console.log(multipliers);
+                console.log(style.getPropertyValue('--duration'));
+            })();
+        
+        popper.update();
+        
         if (popup.hasClass('fade')) {
             popup.css('display', 'block');
-
+            
             timeout = setTimeout(function () {
                 popup.removeClass('fade');
                 clearTimeout(timeout);
                 timeout = null;
-            }, 50);
+            }, delay);
         } else {
             popup.addClass('fade');
-
+            
             timeout = setTimeout(function () {
                 popup.css('display', 'none');
                 clearTimeout(timeout);
                 timeout = null;
-            }, 250);
+            }, 1050);
         }
     })
 });
