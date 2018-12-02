a0a8ae86641b85325152765edf1f3cbf1505a3a4
fomichev <fomichev@nca.by>
2018-12-03 00:53:46 +0300

    read-key > ctrl+f1 = pure magic slider tree proto

---
+++ projects/tree/js/tree-script.js
@@ -0,0 +1,63 @@
+/**
+ * Created by Fomichev Yuri on 19.10.2017
+ * Contact me at : toki.stamp@gmail.com
+ */
+
+/* jQuery */
+jQuery(document).ready(function main() {
+    let tableContainer  = $('.table-container'),
+        items           = tableContainer.find('.item'),
+        alertsContainer = $('.tree-alerts-container'),
+        additionsTable  = $('#tableOfAdditionalFeatures_'),
+        addBtn          = additionsTable.find('.btn-warning');
+    
+    // items.on('click', function () {
+    //     $('<div>', {
+    //         'id'   : $(this).text(),
+    //         'class': 'tree-alert',
+    //         'title': 'Click to remove!',
+    //         'text' : 'Clicked on ' + $(this).text()
+    //     }).appendTo(alertsContainer);
+    // });
+    
+    alertsContainer.on('click', '.tree-alert', function () {
+        $(this).remove();
+    });
+    
+    tableContainer.get(0).addEventListener('change', function (event) {
+        let target = $(event.target);
+        
+        if (target.is('input, select')) {
+            $('<div>', {
+                'id'   : target.text(),
+                'class': 'tree-alert',
+                'title': 'Click to remove!',
+                'text' : 'Change event on ' + target.attr('id')
+            })
+            .appendTo(alertsContainer);
+        }
+        
+        /* блокировать дальнейшее погружение */
+        //event.stopImmediatePropagation();
+        console.log(event);
+    }, true);
+    tableContainer.on('click', 'tr', function () {
+        let me      = $(this),
+            parent  = me.parent(),
+            headers = parent.find('tr:eq(0), tr:eq(1)');
+        
+        if (!me.is(headers)) {
+            $(this).toggleClass('active');
+        }
+    });
+    
+    addBtn.on('click', function () {
+        let last = tableContainer.find('tr').last().clone();
+        
+        last.appendTo(additionsTable);
+    });
+});
+
+/* Self-invoking function */
+(function main() {
+})();
