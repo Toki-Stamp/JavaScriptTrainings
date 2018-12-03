dcd8422f585bec328bb8ff8d5aa3028757358385
Yuri Fomichev <fomichev@nca.by>
2018-12-03 09:46:20 +0300

    minor changes

--- projects/tree/js/tree-script.js
+++ projects/tree/js/tree-script.js
@@ -9,8 +9,8 @@ jQuery(document).ready(function main() {
         items           = tableContainer.find('.item'),
         alertsContainer = $('.tree-alerts-container'),
         additionsTable  = $('#tableOfAdditionalFeatures_'),
-        addBtn          = additionsTable.find('.btn-warning');
-    
+        addBtn          = additionsTable.find('[data-role="add"]');
+
     // items.on('click', function () {
     //     $('<div>', {
     //         'id'   : $(this).text(),
@@ -19,24 +19,24 @@ jQuery(document).ready(function main() {
     //         'text' : 'Clicked on ' + $(this).text()
     //     }).appendTo(alertsContainer);
     // });
-    
+
     alertsContainer.on('click', '.tree-alert', function () {
         $(this).remove();
     });
-    
+
     tableContainer.get(0).addEventListener('change', function (event) {
         let target = $(event.target);
-        
+
         if (target.is('input, select')) {
             $('<div>', {
-                'id'   : target.text(),
+                'id':    target.text(),
                 'class': 'tree-alert',
                 'title': 'Click to remove!',
-                'text' : 'Change event on ' + target.attr('id')
+                'text':  'Change event on ' + target.attr('id')
             })
-            .appendTo(alertsContainer);
+                .appendTo(alertsContainer);
         }
-        
+
         /* блокировать дальнейшее погружение */
         //event.stopImmediatePropagation();
         console.log(event);
@@ -45,19 +45,15 @@ jQuery(document).ready(function main() {
         let me      = $(this),
             parent  = me.parent(),
             headers = parent.find('tr:eq(0), tr:eq(1)');
-        
+
         if (!me.is(headers)) {
             $(this).toggleClass('active');
         }
     });
-    
+
     addBtn.on('click', function () {
         let last = tableContainer.find('tr').last().clone();
-        
+
         last.appendTo(additionsTable);
     });
 });
-
-/* Self-invoking function */
-(function main() {
-})();
