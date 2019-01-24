ae235bc1df810b37faa3a07aa5260ceec7a74e91
fomichev <fomichev@nca.by>
2019-01-25 01:27:16 +0300

    table responsive layout

--- projects/table-layout/js/table-layout-script-2.js
+++ projects/table-layout/js/table-layout-script-2.js
@@ -1,27 +1,41 @@
 /* jQuery */
 jQuery(document).ready(function main() {
     /**
-     *  Добавляем атрибут data-label ко всем элементам td,
+     *  1. Добавляем атрибут data-label ко всем элементам td,
      *  со значением взятым из th (#name) того же столбца, в котором находится td.
      *  Используется для создания меток ячеек таблицы при минималистичном отображении
+     *
+     *  2. Оборачиваем текстовые данные внутри элементов td конструкцией
+     *  <div class="..."></div>, с целью фиксирования параметра height элемента td
      * */
     $('.main-table td').each(function (index, item) {
-        $(item).attr('data-label', (function () {
+        let element = $(item),
+            text    = element.text();
+        
+        element
+        .attr('data-label', (function () {
             let map = {};
-
+            
             $('.main-table #name th').each(function (index, item) {
                 if (!map.hasOwnProperty(map[index])) {
                     map[index] = item.textContent;
                 }
             });
-
+            
             return map;
         })()[item.cellIndex])
+        .html('<div class="x-data-wrapper">' + text + '</div>');
     });
-
+    
     $('.main-table').on('click', 'tbody tr', function (e) {
         $(e.currentTarget).addClass('active').siblings().removeClass('active');
     });
+    
+    // $('.main-table td').on('mouseover', function (event) {
+    //     $(this).find('div.x-data-wrapper').css('max-height', '100%');
+    // }).on('mouseout', function (event) {
+    //     $(this).find('div.x-data-wrapper').css('max-height', '80px');
+    // });
 });
 
 /* Self-invoking function */
