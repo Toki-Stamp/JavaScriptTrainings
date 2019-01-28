1f7dfd6dd012e23b6c10f62c4a8f63dd6279a29c
fomichev <fomichev@nca.by>
2019-01-29 00:05:11 +0300

    parallel scroll

--- projects/table-layout/js/table-layout-script-2.js
+++ projects/table-layout/js/table-layout-script-2.js
@@ -8,28 +8,37 @@ jQuery(document).ready(function main() {
      *  2. Оборачиваем текстовые данные внутри элементов td конструкцией
      *  <div class="..."></div>, с целью фиксирования параметра height элемента td
      * */
-    $('.table-body td').each(function (index, item) {
+    $('.table-body td, .table-header th').each(function (index, item) {
         let element = $(item),
             text    = element.text();
-
+        
         element
-            .attr('data-label', (function () {
-                let map = {};
-
-                $('.main-table #name th').each(function (index, item) {
-                    if (!map.hasOwnProperty(map[index])) {
-                        map[index] = item.textContent;
-                    }
-                });
-
-                return map;
-            })()[item.cellIndex])
-            .html('<div class="x-data-wrapper">' + text + '</div>');
+        .attr('data-label', (function () {
+            let map = {};
+            
+            $('.main-table #name th').each(function (index, item) {
+                if (!map.hasOwnProperty(map[index])) {
+                    map[index] = item.textContent;
+                }
+            });
+            
+            return map;
+        })()[item.cellIndex])
+        .html('<div class="x-data-wrapper">' + text + '</div>');
     });
-
+    
     $('.main-table').on('click', 'tbody tr', function (e) {
         $(e.currentTarget).addClass('active').siblings().removeClass('active');
     });
+    
+    (function () {
+        var target = $(".table-header")[0]; // <== Getting raw element
+        
+        $(".table-body").scroll(function () {
+            target.scrollTop = this.scrollTop;
+            target.scrollLeft = this.scrollLeft;
+        });
+    })();
 });
 
 /* Self-invoking function */
