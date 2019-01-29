08591c16b139a38b8afddabde1285d3b16da1521
fomichev <fomichev@nca.by>
2019-01-30 02:33:16 +0300

    parallel scroll

--- projects/table-layout/js/table-layout-script-2.js
+++ projects/table-layout/js/table-layout-script-2.js
@@ -1,5 +1,6 @@
 /* jQuery */
 jQuery(document).ready(function main() {
+    let tableContainer = $('.x-table-container');
     /**
      *  1. Добавляем атрибут data-label ко всем элементам td,
      *  со значением взятым из th (#name) того же столбца, в котором находится td.
@@ -8,22 +9,22 @@ jQuery(document).ready(function main() {
      *  2. Оборачиваем текстовые данные внутри элементов td конструкцией
      *  <div class="..."></div>, с целью фиксирования параметра height элемента td
      * */
-    $('.x-table-container td, .x-table-container th').each(function (index, item) {
+    tableContainer.find('td, th').each(function (index, item) {
         let element = $(item),
             wrap,
             text    = element.text(),
             html    = element.html();
-
+        
         if (element.is('td')) {
             element.attr('data-label', (function () {
                 let map = {};
-
+                
                 $('#name').find('th').each(function (index, item) {
                     if (!map.hasOwnProperty(map[index])) {
                         map[index] = item.textContent;
                     }
                 });
-
+                
                 return map;
             })()[item.cellIndex])
         }
@@ -32,13 +33,13 @@ jQuery(document).ready(function main() {
         } else {
             wrap = text;
         }
-
+        
         element.html('<div class="x-data-wrapper">' + wrap + '</div>');
     });
-
-    $('.x-table-container').find('tbody').on('click', 'tr', function (e) {
+    
+    tableContainer.find('tbody').on('click', 'tr', function (e) {
         $(e.currentTarget).addClass('active').siblings().removeClass('active');
     });
-
-    $('.x-table-container').find('thead').find('select, input').addClass('form-control');
+    
+    tableContainer.find('thead').find('select, input').addClass('form-control');
 });
