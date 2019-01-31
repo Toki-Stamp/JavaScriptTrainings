9e726a25d840df1ab9c3482c410ec98d8383b421
Yuri Fomichev <fomichev@nca.by>
2019-01-31 15:50:00 +0300

    done?

--- projects/table-layout/js/table-layout-script-2.js
+++ projects/table-layout/js/table-layout-script-2.js
@@ -2,12 +2,12 @@
 jQuery(document).ready(function main() {
     let tableContainer = $('.x-table-container');
     /**
-     *  1. Добавляем атрибут data-label ко всем элементам td,
-     *  со значением взятым из th (#name) того же столбца, в котором находится td.
+     *  1. Добавляем атрибут data-label ко всем элементам x-data-wrapper,
+     *  со значением взятым из th (#header-title) того же столбца, в котором находится div.
      *  Используется для создания меток ячеек таблицы при минималистичном отображении
      *
      *  2. Оборачиваем текстовые данные внутри элементов td конструкцией
-     *  <div class="..."></div>, с целью фиксирования параметра height элемента td
+     *  <div class="..."></div>, с целью фиксирования параметров контейнера
      * */
     tableContainer.find('td, th').each(function (index, item) {
         let element = $(item),
@@ -15,19 +15,6 @@ jQuery(document).ready(function main() {
             text    = element.text(),
             html    = element.html();
         
-        if (element.is('td')) {
-            element.attr('data-label', (function () {
-                let map = {};
-                
-                $('#name').find('th').each(function (index, item) {
-                    if (!map.hasOwnProperty(map[index])) {
-                        map[index] = item.textContent;
-                    }
-                });
-                
-                return map;
-            })()[item.cellIndex])
-        }
         if (element.is('.x-table-container thead tr:eq(2) th')) {
             wrap = html;
         } else {
@@ -35,6 +22,20 @@ jQuery(document).ready(function main() {
         }
         
         element.html('<div class="x-data-wrapper">' + wrap + '</div>');
+
+        if (element.is('td')) {
+            element.find('.x-data-wrapper').attr('data-label', (function () {
+                let map = {};
+
+                $('#header-title').find('th').each(function (index, item) {
+                    if (!map.hasOwnProperty(map[index])) {
+                        map[index] = item.textContent;
+                    }
+                });
+
+                return map;
+            })()[item.cellIndex])
+        }
     });
     
     tableContainer.find('tbody').on('click', 'tr', function (e) {
