fcd2dced6329a2b96837db86efa46758ad213109
fomichev <fomichev@nca.by>
2019-02-11 00:37:01 +0300

    parallel scroll

--- projects/rh_utils/js/rh_utils.js
+++ projects/rh_utils/js/rh_utils.js
@@ -3,87 +3,95 @@
  * Contact me at : toki.stamp@gmail.com
  */
 
-/**
- * Created by Fomichev Yuri on 08.02.2019
- * Contact me at : toki.stamp@gmail.com
- */
 (function init_utils(referenceName) {
     var utils = (window[referenceName] || {});
-
+    
     function /* constructor */ Shield() {
-        var thisMessage = 'Загрузка данных...',
-            classes     = {
-                'shield':   'x-shield-container',
-                'body':     'x-body',
+        var classes   = {
+                'shield'  : 'x-shield-container',
+                'body'    : 'x-body',
                 'relative': 'x-relative',
-                'text':     'x-shield-text'
+                'text'    : 'x-shield-text',
+                'get'     : function (className) {
+                    return ['.', this[className]].join('');
+                }
             },
-            reference   = {'target': null};
-
-        function createShield(className, message) {
-            var shieldScreen       = $('<div>', {'class': (className || classes['shield'])}),
+            reference = {'target': null, 'shield': null, 'message': 'Загрузка данных...'};
+        /* chain call */
+        this.create = function () {
+            var shieldScreen       = $('<div>', {'class': classes['shield']}),
                 shieldMessage      = $('<div>', {'class': 'x-shield-message'}),
                 animationContainer = $('<div>', {'class': 'x-shield-message-animation-container'}),
                 textContainer      = $('<div>', {'class': 'x-shield-message-text-container'}),
                 spinnerAnimation   = $('<span>', {'class': 'fa fa-spinner fa-pulse'}),
-                shieldText         = $('<span>', {'class': classes['text'], 'text': (message || thisMessage)});
-
+                shieldText         = $('<span>', {'class': classes['text'], 'text': reference['message']});
+            
             animationContainer.appendTo(shieldMessage);
             textContainer.appendTo(shieldMessage);
             spinnerAnimation.appendTo(animationContainer);
             shieldText.appendTo(textContainer);
             shieldMessage.appendTo(shieldScreen);
-
-            return shieldScreen;
-        }
-
-        this.bind    = function (target) {
+            
+            reference['shield'] = shieldScreen;
+            
+            return this;
+        };
+        this.bind = function (target) {
             reference['target'] = target;
-
+            
             return this;
         };
         this.message = function (message) {
-            reference['target'].find(['.', classes['text']].join('')).text(message);
-
+            reference['message'] = message;
+            reference['shield'].find(classes.get('text')).text(message);
+            
             return this;
         };
-        //create
-        this.rise    = function () {
+        this.rise = function () {
             if (reference['target']) {
                 if (reference['target'].is('body')) {
                     reference['target'].addClass(classes['body']);
                 } else {
                     reference['target'].addClass(classes['relative']);
                 }
-
-                createShield().prependTo(reference['target']);
+                
+                reference['shield'].prependTo(reference['target']);
             }
-
+            
             return this;
         };
         this.decline = function () {
             if (reference['target']) {
                 reference['target'].removeClass(classes['relative']).removeClass(classes['body']);
-                reference['target'].find(['.', classes['shield']].join('')).remove();
+                reference['target'].find(classes.get('shield')).remove();
             }
+            
+            return this;
+        };
+        this.status = function () {
+            console.info('status: ', reference);
         };
     }
-
+    
     utils.shield = (function () {
         var counter = 0,
             store   = {};
-
+        
         return {
-            getShield: function () {
+            getNewInstance: function () {
                 var instance = new Shield();
-
+                
                 store[++counter] = instance;
-
+                
                 return instance;
+            },
+            status        : function () {
+                console.info('instances: ', counter);
+                console.info('store: ', store);
             }
         }
     })();
-
+    
     /* return */
     window[referenceName] = utils;
 })('rh_utils');
