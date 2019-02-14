ab766d096a3901f45c9c258552f02cff44a8e34b
Yuri Fomichev <fomichev@nca.by>
2019-02-14 19:36:36 +0300

    доработки rh_utils

--- projects/rh_utils/js/rh_utils.js
+++ projects/rh_utils/js/rh_utils.js
@@ -5,19 +5,20 @@
 
 (function init_utils(referenceName) {
     var utils = (window[referenceName] || {});
-    
+
     function /* constructor */ Shield() {
         var classes   = {
                 'shield'  : 'x-shield-container',
                 'body'    : 'x-body',
                 'relative': 'x-relative',
                 'text'    : 'x-shield-text',
+                'hidden'  : 'x-hidden',
                 'get'     : function (className) {
                     return ['.', this[className]].join('');
                 }
             },
             reference = {'target': null, 'shield': null, 'message': 'Загрузка данных...'};
-        /* chain call */
+        /* chain call pattern */
         this.create = function () {
             var shieldScreen       = $('<div>', {'class': classes['shield']}),
                 shieldMessage      = $('<div>', {'class': 'x-shield-message'}),
@@ -25,64 +26,83 @@
                 textContainer      = $('<div>', {'class': 'x-shield-message-text-container'}),
                 spinnerAnimation   = $('<span>', {'class': 'fa fa-spinner fa-pulse'}),
                 shieldText         = $('<span>', {'class': classes['text'], 'text': reference['message']});
-            
+
             animationContainer.appendTo(shieldMessage);
             textContainer.appendTo(shieldMessage);
             spinnerAnimation.appendTo(animationContainer);
             shieldText.appendTo(textContainer);
             shieldMessage.appendTo(shieldScreen);
-            
+
             reference['shield'] = shieldScreen;
-            
+
             return this;
         };
         this.bind = function (target) {
             reference['target'] = target;
-            
+
             return this;
         };
         this.message = function (message) {
             reference['message'] = message;
             reference['shield'].find(classes.get('text')).text(message);
-            
+
             return this;
         };
-        this.rise = function () {
+        this.show = function () {
             if (reference['target']) {
                 if (reference['target'].is('body')) {
-                    reference['target'].addClass(classes['body']);
+                    if (!reference['target'].hasClass(classes['body'])) {
+                        reference['target'].addClass(classes['body']);
+                    }
                 } else {
-                    reference['target'].addClass(classes['relative']);
+                    if (!reference['target'].hasClass(classes['relative'])) {
+                        reference['target'].addClass(classes['relative']);
+                    }
                 }
-                
-                reference['shield'].prependTo(reference['target']);
+
+                if (reference['shield'].hasClass(classes['hidden'])) {
+                    reference['shield'].removeClass(classes['hidden']);
+                } else {
+                    if (!reference['target'].find(classes.get('shield')).length) {
+                        reference['shield'].prependTo(reference['target']);
+                    }
+                }
             }
-            
+
             return this;
         };
-        this.decline = function () {
+        this.hide = function () {
+            if (reference['shield']) {
+                if (!reference['shield'].hasClass(classes['hidden'])) {
+                    reference['shield'].addClass(classes['hidden']);
+                }
+            }
+
+            return this;
+        };
+        this.destroy = function () {
             if (reference['target']) {
                 reference['target'].removeClass(classes['relative']).removeClass(classes['body']);
                 reference['target'].find(classes.get('shield')).remove();
             }
-            
+
             return this;
         };
         this.status = function () {
             console.info('status: ', reference);
         };
     }
-    
+
     utils.shield = (function () {
         var counter = 0,
             store   = {};
-        
+
         return {
             getNewInstance: function () {
                 var instance = new Shield();
-                
+
                 store[++counter] = instance;
-                
+
                 return instance;
             },
             status        : function () {
@@ -91,7 +111,7 @@
             }
         }
     })();
-    
+
     /* return */
     window[referenceName] = utils;
 })('rh_utils');
