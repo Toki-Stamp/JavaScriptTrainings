835b47edef65b23f13d1b533fdc0634f0863b485
Yuri Fomichev <fomichev@nca.by>
2019-05-22 15:34:11 +0300

    alert done!

--- projects/rh_utils/js/rh_utils.js
+++ projects/rh_utils/js/rh_utils.js
@@ -5,57 +5,57 @@
 
 (function init_utils(referenceName) {
     var utils = (window[referenceName] || {});
-    
+
     function /* constructor */ Shield() {
         var classes   = {
-                'shield'  : 'x-shield-container',
-                'message' : 'x-shield-message',
-                'body'    : 'x-body',
+                'shield':   'x-shield-container',
+                'message':  'x-shield-message',
+                'body':     'x-body',
                 'relative': 'x-relative',
-                'text'    : 'x-shield-text',
-                'hidden'  : 'x-hidden',
-                'get'     : function (className) {
+                'text':     'x-shield-text',
+                'hidden':   'x-hidden',
+                'get':      function (className) {
                     return ['.', this[className]].join('');
                 }
             },
             reference = {'target': null, 'shield': null, 'message': null};
         /* chain call pattern */
-        this.create = function () {
+        this.create  = function () {
             var shieldScreen       = $('<div>', {'class': classes['shield']}),
                 shieldMessage      = $('<div>', {'class': classes['message']}),
                 animationContainer = $('<div>', {'class': 'x-shield-message-animation-container'}),
                 textContainer      = $('<div>', {'class': 'x-shield-message-text-container'}),
                 spinnerAnimation   = $('<span>', {'class': 'fa fa-spinner fa-pulse'}),
                 shieldText         = $('<span>', {'class': classes['text'], 'text': reference['message']});
-            
+
             animationContainer.appendTo(shieldMessage);
             textContainer.appendTo(shieldMessage);
             spinnerAnimation.appendTo(animationContainer);
             shieldText.appendTo(textContainer);
             shieldMessage.appendTo(shieldScreen);
-            
+
             reference['shield'] = shieldScreen;
-            
+
             return this;
         };
-        this.bind = function (target) {
+        this.bind    = function (target) {
             reference['target'] = target;
-            
+
             return this;
         };
         this.message = function (message) {
             reference['message'] = message;
             reference['shield'].find(classes.get('text')).text(message);
-            
+
             if (reference['message']) {
                 reference['shield'].find(classes.get('message')).removeClass(classes['hidden']);
             } else {
                 reference['shield'].find(classes.get('message')).addClass(classes['hidden']);
             }
-            
+
             return this;
         };
-        this.show = function () {
+        this.show    = function () {
             if (reference['target']) {
                 if (reference['target'].is('body')) {
                     if (!reference['target'].hasClass(classes['body'])) {
@@ -66,13 +66,13 @@
                         reference['target'].addClass(classes['relative']);
                     }
                 }
-                
+
                 if (reference['message']) {
                     reference['shield'].find(classes.get('message')).removeClass(classes['hidden']);
                 } else {
                     reference['shield'].find(classes.get('message')).addClass(classes['hidden']);
                 }
-                
+
                 if (reference['shield'].hasClass(classes['hidden'])) {
                     reference['shield'].removeClass(classes['hidden']);
                 } else {
@@ -81,16 +81,16 @@
                     }
                 }
             }
-            
+
             return this;
         };
-        this.hide = function () {
+        this.hide    = function () {
             if (reference['shield']) {
                 if (!reference['shield'].hasClass(classes['hidden'])) {
                     reference['shield'].addClass(classes['hidden']);
                 }
             }
-            
+
             return this;
         };
         this.destroy = function () {
@@ -98,36 +98,198 @@
                 reference['target'].removeClass(classes['relative']).removeClass(classes['body']);
                 reference['target'].find(classes.get('shield')).remove();
             }
-            
+
             return this;
         };
-        this.status = function () {
+        this.status  = function () {
             console.info('status: ', reference);
         };
     }
+
     function /* constructor */ Alert() {
-    
+        let reference = (function () {
+            let description = {
+                'target':  $('body'),
+                'modal':   null,
+                'title':   {
+                    'defaultValue': 'Подтвердите действие',
+                    'currentValue': null,
+                    'reactive':     true,
+                    'callback':     function (title) {
+                        if (description['modal'] && description['modal'].length) {
+                            description['modal'].find('.modal-title').text(((title && title.length) ? title : description['title']['defaultValue']));
+                        }
+                    }
+                },
+                'message': {
+                    'defaultValue': 'Пустое сообщение...',
+                    'currentValue': null,
+                    'reactive':     true,
+                    'callback':     function (message) {
+                        if (description['modal'] && description['modal'].length) {
+                            description['modal'].find('.modal-body').html(message);
+                        }
+                    }
+                },
+            };
+
+            Object.keys(description).forEach(item => {
+                let internalValue;
+
+                if (description[item] && description[item]['reactive']) {
+                    internalValue = description[item]['currentValue'];
+
+                    Object.defineProperty(description[item], 'currentValue', {
+                        get() {
+                            // console.log(`Прочитали "${item}" равное "${internalValue}"`);
+
+                            return internalValue;
+                        },
+                        set(newValue) {
+                            let preValue = ((newValue && newValue.length) ? newValue : null);
+
+                            // console.log(`Установили "${item}" из "${internalValue}" в значение "${preValue}"`);
+                            internalValue = preValue;
+                            description[item]['callback'](preValue);
+                        }
+                    });
+                }
+            });
+
+            return description;
+        })();
+
+        this.create  = function () {
+            let modal,
+                modalDialog,
+                modalContent,
+                modalHeader,
+                modalHeaderContent,
+                modalBody,
+                modalFooter,
+                modalFooterContent,
+                key;
+
+            if (reference && reference['modal']) {
+                return this;
+            }
+
+            modal              = $('<div>', {'class': 'modal fade', 'id': 'rh-alert', 'tabindex': '-1', 'role': 'dialog'});
+            modalDialog        = $('<div>', {'class': 'modal-dialog', 'role': 'document'});
+            modalContent       = $('<div>', {'class': 'modal-content'});
+            modalHeader        = $('<div>', {'class': 'modal-header'});
+            modalHeaderContent = {
+                'title': $('<h4>', {
+                    'class': 'modal-title',
+                    'html':  reference['title']['defaultValue']
+                })
+            };
+            modalBody          = $('<div>', {
+                'class': 'modal-body',
+                'html':  reference['message']['defaultValue']
+
+            });
+            modalFooter        = $('<div>', {'class': 'modal-footer'});
+            modalFooterContent = {
+                'ok-btn': $('<button>', {
+                    'class':        'btn btn-primary',
+                    'type':         'button',
+                    'data-dismiss': 'modal',
+                    'html':         '&nbsp;&nbsp;OK&nbsp;&nbsp;'
+                })
+            };
+            /* header */
+            for (key in modalHeaderContent) {
+                if (modalHeaderContent.hasOwnProperty(key)) {
+                    modalHeaderContent[key].appendTo(modalHeader);
+                }
+            }
+            modalHeader.appendTo(modalContent);
+            /* body */
+            modalBody.appendTo(modalContent);
+            /* footer */
+            for (key in modalFooterContent) {
+                if (modalFooterContent.hasOwnProperty(key)) {
+                    modalFooterContent[key].appendTo(modalFooter);
+                }
+            }
+            modalFooter.appendTo(modalContent);
+            /* content */
+            modalContent.appendTo(modalDialog);
+            /* dialog */
+            modalDialog.appendTo(modal);
+            /* init bootstrap modal */
+            modal.modal({
+                // 'backdrop': 'static',
+                'backdrop': false,
+                'keyboard': true,
+                'show':     false
+            });
+            modal.on('hidden.bs.modal', function (e) {
+            });
+            modal.prependTo(reference['target']);
+            reference['modal'] = modal;
+
+            return this;
+        };
+        this.message = function (message) {
+            reference['message']['currentValue'] = message;
+
+            return this;
+        };
+        this.title   = function (title) {
+            reference['title']['currentValue'] = title;
+
+            return this;
+        };
+        this.show    = function () {
+            if (reference &&
+                reference['target'] && reference['target'].length &&
+                reference['modal'] && reference['modal'].length &&
+                reference['message'] && reference['message']['currentValue']) {
+                reference['modal'].modal('show');
+            }
+
+            return this;
+        };
+        this.status  = function () {
+            console.info('status: ', reference);
+        };
     }
-    
+
+    utils.alert  = (function () {
+        var instance;
+
+        return {
+            getInstance: function () {
+                /* singleton */
+                if (!instance) {
+                    instance = new Alert();
+                }
+
+                return instance;
+            }
+        }
+    })();
     utils.shield = (function () {
         var counter = 0,
             store   = {};
-        
+
         return {
             getNewInstance: function () {
                 var instance = new Shield();
-                
+
                 store[++counter] = instance;
-                
+
                 return instance;
             },
-            status        : function () {
+            status:         function () {
                 console.info('instances: ', counter);
                 console.info('store: ', store);
             }
         }
     })();
-    
+
     /* return */
     window[referenceName] = utils;
 })('rh_utils');
--- projects/rh_utils/js/rh_utils_modal.js
+++ projects/rh_utils/js/rh_utils_modal.js
@@ -3,24 +3,132 @@
  * Contact me at : toki.stamp@gmail.com
  */
 
-(function sif() {
-    class MyClass {
-        constructor() {
-            this.text = 'Default text...';
+(function () {
+    var rhAlert       = window['rh_utils'].alert.getInstance(),
+        buttons       = $('#x-controls').find('.btn-group').find('button'),
+        setTitle      = $('#set-title'),
+        setMessage    = $('#set-message'),
+        messages      = [
+            'Внимание!!!\n',
+            'Внимание!!!\n' +
+            'У данного объекта (ID 40337291) не завершена операция по удалению: \n' +
+            '   - Заказ №100297746;\n' +
+            '\n' +
+            'Таким образом, создание повторной операции «Удаление объекта» невозможно.',
+            'Внимание!!!\n' +
+            'У данного объекта имеются связанные объекты (всего 71 шт):\n' +
+            '   - ID 40236247;\n' +
+            '   - ID 40236250;\n' +
+            '   - ID 40236255;\n' +
+            '   - ID 40236258;\n' +
+            '   - ID 40236259;\n' +
+            '   - ID 40236262;\n' +
+            '   - ID 40236264;\n' +
+            '   - ID 40236265;\n' +
+            '   - ID 40236267;\n' +
+            '   - ID 40236269;\n' +
+            '   - ID 40236270;\n' +
+            '   - ID 40236271;\n' +
+            '   - ID 40237319;\n' +
+            '   - ID 40237322;\n' +
+            '   - ID 40237323;\n' +
+            '   - ID 40237327;\n' +
+            '   - ID 40237332;\n' +
+            '   - ID 40237333;\n' +
+            '   - ID 40237337;\n' +
+            '   - ID 40237339;\n' +
+            '   - ID 40237794;\n' +
+            '   - ID 40237796;\n' +
+            '   - ID 40237797;\n' +
+            '   - ID 40237798;\n' +
+            '   - ID 40237799;\n' +
+            '   - ID 40237800;\n' +
+            '   - ID 40237801;\n' +
+            '   - ID 40237802;\n' +
+            '   - ID 40237804;\n' +
+            '   - ID 40237805;\n' +
+            '   - ID 40237806;\n' +
+            '   - ID 40237807;\n' +
+            '   - ID 40237809;\n' +
+            '   - ID 40237810;\n' +
+            '   - ID 40237811;\n' +
+            '   - ID 40237812;\n' +
+            '   - ID 40237814;\n' +
+            '   - ID 40237815;\n' +
+            '   - ID 40237816;\n' +
+            '   - ID 40237817;\n' +
+            '   - ID 40237818;\n' +
+            '   - ID 40237819;\n' +
+            '   - ID 40237820;\n' +
+            '   - ID 40237821;\n' +
+            '   - ID 40237822;\n' +
+            '   - ID 40237823;\n' +
+            '   - ID 40237824;\n' +
+            '   - ID 40237825;\n' +
+            '   - ID 40237847;\n' +
+            '   - ID 40237848;\n' +
+            '   - ID 40237849;\n' +
+            '   - ID 40237850;\n' +
+            '   - ID 40237851;\n' +
+            '   - ID 40237854;\n' +
+            '   - ID 40237857;\n' +
+            '   - ID 40237859;\n' +
+            '   - ID 40237861;\n' +
+            '   - ID 40237863;\n' +
+            '   - ID 40237865;\n' +
+            '   - ID 40237869;\n' +
+            '   - ID 40240896;\n' +
+            '   - ID 40240899;\n' +
+            '   - ID 40240903;\n' +
+            '   - ID 40240906;\n' +
+            '   - ID 40240908;\n' +
+            '   - ID 40240909;\n' +
+            '   - ID 40240912;\n' +
+            '   - ID 40240914;\n' +
+            '   - ID 40240918;\n' +
+            '   - ID 40240922;\n' +
+            '   - ID 40240923;\n' +
+            '\n' +
+            'Таким образом, для удаления данного объекта, сперва необходимо удалить (либо назначить нового родителя) всем его связанным объектам.'],
+        randomInteger = function (min, max) {
+            var rand = min - 0.5 + Math.random() * (max - min + 1);
+
+            return Math.round(rand);
+        };
+
+    buttons.first().on('click', function (e) {
+        window.alert('Hello, World!');
+    });
+
+    buttons.last().on('click', function (e) {
+        var message = setMessage.find('input').val();
+
+        if (!message || (message && !message.length)) {
+            message = messages[randomInteger(0, (messages.length - 1))];
         }
 
-        get text() {
-            console.log('Getter for text!');
+        rhAlert.create().message(message).show().status();
+    });
+
+    setTitle.find('button').first().on('click', function (e) {
+        rhAlert.title(setTitle.find('input').val());
+    });
+    setTitle.find('button').last().on('click', function (e) {
+        setTitle.find('input').val('');
+        rhAlert.title(null);
+    });
+
+    setMessage.find('button').first().on('click', function (e) {
+        var message = setMessage.find('input').val();
+
+        if (!message || (message && !message.length)) {
+            message = messages[randomInteger(0, (messages.length - 1))];
         }
 
-        set text(v) {
-            console.log('Setter for text!');
-        }
-    }
-
-    const object = new MyClass();
-
-    console.log('sif');
-    object.text = 15;
-    console.log(object.text);
+        rhAlert.message(message);
+    });
+    setMessage.find('button').last().on('click', function (e) {
+        rhAlert.message(null);
+        setMessage.find('input').val('');
+    });
 })();
