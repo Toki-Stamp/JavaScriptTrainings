36e9b1a35611c34d5c1cd5604f5a9e2b7ef66280
Yuri Fomichev <fomichev@nca.by>
2019-05-23 16:11:29 +0300

    alfirm!

--- projects/rh_utils/js/rh_utils.js
+++ projects/rh_utils/js/rh_utils.js
@@ -5,57 +5,57 @@
 
 (function init_utils(referenceName) {
     var utils = (window[referenceName] || {});
-    
+
     function /* constructor */ Shield() {
         var classes   = {
-                'shield'  : 'x-shield-container',
-                'message' : 'x-shield-message',
-                'body'    : 'x-body',
+                'shield':   'x-shield-container',
+                'message':  'x-shield-message',
+                'body':     'x-body',
                 'relative': 'x-relative',
-                'text'    : 'x-shield-text',
-                'hidden'  : 'x-hidden',
-                'get'     : function (className) {
+                'text':     'x-shield-text',
+                'hidden':   'x-hidden',
+                'get':      function (className) {
                     return ['.', this[className]].join('');
                 }
             },
             reference = {'target': null, 'shield': null, 'message': null};
         /* chain call pattern */
-        this.create = function () {
+        this.create  = function () {
             var shieldScreen       = $('<div>', {'class': classes['shield']}),
                 shieldMessage      = $('<div>', {'class': classes['message']}),
                 animationContainer = $('<div>', {'class': 'x-shield-message-animation-container'}),
                 textContainer      = $('<div>', {'class': 'x-shield-message-text-container'}),
                 spinnerAnimation   = $('<span>', {'class': 'fa fa-spinner fa-pulse'}),
                 shieldText         = $('<span>', {'class': classes['text'], 'text': reference['message']});
-            
+
             animationContainer.appendTo(shieldMessage);
             textContainer.appendTo(shieldMessage);
             spinnerAnimation.appendTo(animationContainer);
             shieldText.appendTo(textContainer);
             shieldMessage.appendTo(shieldScreen);
-            
+
             reference['shield'] = shieldScreen;
-            
+
             return this;
         };
-        this.bind = function (target) {
+        this.bind    = function (target) {
             reference['target'] = target;
-            
+
             return this;
         };
         this.message = function (message) {
             reference['message'] = message;
             reference['shield'].find(classes.get('text')).text(message);
-            
+
             if (reference['message']) {
                 reference['shield'].find(classes.get('message')).removeClass(classes['hidden']);
             } else {
                 reference['shield'].find(classes.get('message')).addClass(classes['hidden']);
             }
-            
+
             return this;
         };
-        this.show = function () {
+        this.show    = function () {
             if (reference['target']) {
                 if (reference['target'].is('body')) {
                     if (!reference['target'].hasClass(classes['body'])) {
@@ -66,13 +66,13 @@
                         reference['target'].addClass(classes['relative']);
                     }
                 }
-                
+
                 if (reference['message']) {
                     reference['shield'].find(classes.get('message')).removeClass(classes['hidden']);
                 } else {
                     reference['shield'].find(classes.get('message')).addClass(classes['hidden']);
                 }
-                
+
                 if (reference['shield'].hasClass(classes['hidden'])) {
                     reference['shield'].removeClass(classes['hidden']);
                 } else {
@@ -81,16 +81,16 @@
                     }
                 }
             }
-            
+
             return this;
         };
-        this.hide = function () {
+        this.hide    = function () {
             if (reference['shield']) {
                 if (!reference['shield'].hasClass(classes['hidden'])) {
                     reference['shield'].addClass(classes['hidden']);
                 }
             }
-            
+
             return this;
         };
         this.destroy = function () {
@@ -98,225 +98,141 @@
                 reference['target'].removeClass(classes['relative']).removeClass(classes['body']);
                 reference['target'].find(classes.get('shield')).remove();
             }
-            
+
             return this;
         };
-        this.status = function () {
+        this.status  = function () {
             console.info('status: ', reference);
         };
     }
-    
-    function /* constructor */ Alert() {
-        var reference = (function () {
-            var description = {
-                'target' : $('body'),
-                'modal'  : null,
-                'title'  : {
-                    'defaultValue': 'Подтвердите действие',
-                    'currentValue': null,
-                    'reactive'    : true,
-                    'callback'    : function (title) {
-                        if (description['modal'] && description['modal'].length) {
-                            description['modal'].find('.modal-title').text(((title && title.length) ? title : description['title']['defaultValue']));
-                        }
-                    }
+
+    function /* constructor */ Alfirm() {
+        var defaultValues = {
+                title:    'Подтвердите действие',
+                body:     'Пустое сообщение...',
+                onOkay:   function () {
+                    console.log('Alfirm Confirmed! [Oк]');
                 },
-                'message': {
-                    'defaultValue': 'Пустое сообщение...',
-                    'currentValue': null,
-                    'reactive'    : true,
-                    'callback'    : function (message) {
-                        if (description['modal'] && description['modal'].length) {
-                            description['modal'].find('.modal-body').html(message);
-                        }
-                    }
+                onCancel: function () {
+                    console.log('Alfirm Canceled! [Отмена]');
                 },
-            };
-            
-            Object.keys(description).forEach(function (item) {
-                var internalValue;
-                
-                if (description[item] && description[item]['reactive']) {
-                    internalValue = description[item]['currentValue'];
-                    
-                    Object.defineProperty(description[item], 'currentValue', {
-                        get: function () {
-                            console.log(`Прочитали "${item}" равное "${internalValue}"`);
-                            
-                            return internalValue;
-                        },
-                        set: function (newValue) {
-                            var preValue = ((newValue && newValue.length) ? newValue : null);
-                            
-                            console.log(`Установили "${item}" из "${internalValue}" в значение "${preValue}"`);
-                            internalValue = preValue;
-                            description[item]['callback'](preValue);
-                        }
-                    });
+                shown:    function (e) {
+                    console.log('Alfirm Shown!');
+                    e.data.focusButton.focus();
+                },
+                hidden:   function (e) {
+                    console.log('Alfirm Hidden!');
                 }
-            });
-            
-            return description;
-        })();
-        
-        this.reference = reference;
-        this.create = function () {
-            var modal,
-                modalDialog,
-                modalContent,
-                modalHeader,
-                modalHeaderContent,
-                modalBody,
-                modalFooter,
-                modalFooterContent,
-                key;
-            
-            if (reference && reference['modal']) {
-                return this;
-            }
-            
-            modal = $('<div>', {'class': 'modal fade', 'id': 'rh-alert', 'tabindex': '-1', 'role': 'dialog'});
-            modalDialog = $('<div>', {'class': 'modal-dialog', 'role': 'document'});
-            modalContent = $('<div>', {'class': 'modal-content'});
-            modalHeader = $('<div>', {'class': 'modal-header'});
-            modalHeaderContent = {
-                'title': $('<h4>', {
-                    'class': 'modal-title',
-                    'html' : reference['title']['defaultValue']
-                })
-            };
-            modalBody = $('<div>', {
-                'class': 'modal-body',
-                'html' : reference['message']['defaultValue']
-                
-            });
-            modalFooter = $('<div>', {'class': 'modal-footer'});
-            modalFooterContent = {
-                'ok-btn': $('<button>', {
-                    'class'       : 'btn btn-primary',
-                    'type'        : 'button',
+            },
+            alfirm        = null;
+
+        function init() {
+            var container    = $('body'),
+                modal        = $('<div>', {'class': 'modal fade', 'id': 'x-alfirm', 'tabindex': '-1', 'role': 'dialog'}),
+                modalDialog  = $('<div>', {'class': 'modal-dialog', 'role': 'document'}),
+                modalContent = $('<div>', {'class': 'modal-content'}),
+                modalHeader  = $('<div>', {'class': 'modal-header'}),
+                modalTitle   = $('<div>', {'class': 'modal-title', 'html': defaultValues.title}),
+                modalBody    = $('<div>', {'class': 'modal-body', 'html': defaultValues.body}),
+                modalFooter  = $('<div>', {'class': 'modal-footer'}),
+                btnOkay      = $('<button>', {
+                    'id':           'x-alfirm-btn-okay',
+                    'class':        'btn btn-primary',
+                    'type':         'button',
                     'data-dismiss': 'modal',
-                    'html'        : '&nbsp;&nbsp;OK&nbsp;&nbsp;'
-                })
-            };
-            /* header */
-            for (key in modalHeaderContent) {
-                if (modalHeaderContent.hasOwnProperty(key)) {
-                    modalHeaderContent[key].appendTo(modalHeader);
-                }
-            }
-            modalHeader.appendTo(modalContent);
-            /* body */
-            modalBody.appendTo(modalContent);
-            /* footer */
-            for (key in modalFooterContent) {
-                if (modalFooterContent.hasOwnProperty(key)) {
-                    modalFooterContent[key].appendTo(modalFooter);
-                }
-            }
-            modalFooter.appendTo(modalContent);
+                    'html':         '&nbsp;&nbsp;Ок&nbsp;&nbsp;'
+                }),
+                btnCancel    = $('<button>', {
+                    'id':           'x-alfirm-btn-cancel',
+                    'class':        'btn btn-default',
+                    'type':         'button',
+                    'data-dismiss': 'modal',
+                    'html':         '&nbsp;&nbsp;Отмена&nbsp;&nbsp;'
+                }),
+                model        = {
+                    reference: modal,
+                    callback:  null,
+                    content:   {
+                        title:  {reference: modalTitle, text: defaultValues.title},
+                        body:   {reference: modalBody, text: defaultValues.body},
+                        footer: {
+                            reference: modalFooter,
+                            buttons:   {
+                                okay:   {reference: btnOkay, callback: defaultValues.onOkay},
+                                cancel: {reference: btnCancel, callback: defaultValues.onCancel},
+                            }
+                        }
+                    }
+                };
+            console.log('Alfirm Init!');
             /* content */
+            modalTitle.appendTo(modalHeader);
+            modalHeader.appendTo(modalContent);
+            modalBody.appendTo(modalContent);
+            btnOkay.appendTo(modalFooter);
+            btnCancel.appendTo(modalFooter);
+            modalFooter.appendTo(modalContent);
             modalContent.appendTo(modalDialog);
-            /* dialog */
             modalDialog.appendTo(modal);
-            /* init bootstrap modal */
-            modal.modal({
-                // 'backdrop': 'static',
-                'backdrop': false,
-                'keyboard': true,
-                'show'    : false
-            });
-            modal.on('hidden.bs.modal', function (e) {
-            });
-            modal.prependTo(reference['target']);
-            reference['modal'] = modal;
-            
-            return this;
-        };
-        this.message = function (message) {
-            reference['message']['currentValue'] = message;
-            
-            return this;
-        };
-        this.title = function (title) {
-            reference['title']['currentValue'] = title;
-            
-            return this;
-        };
-        this.show = function () {
-            console.log('alert show');
-            
-            if (reference &&
-                reference['target'] && reference['target'].length &&
-                reference['modal'] && reference['modal'].length &&
-                reference['message'] && reference['message']['currentValue']) {
-                reference['modal'].modal('show');
+            /* bootstrap: modal */
+            modal.modal({'backdrop': false, 'keyboard': true, 'show': false});
+            modal.on('shown.bs.modal', {focusButton: btnOkay}, defaultValues.shown);
+            modal.on('hidden.bs.modal', {modal: modal}, defaultValues.hidden);
+
+            modal.prependTo(container);
+            console.log('Alfirm Initiated!');
+
+            return model;
+        }
+
+        this.show   = function () {
+            if (!alfirm) {
+                alfirm = init();
             }
-            
+
+            console.log('Alfirm Show!');
+            alfirm.reference.modal('show');
+
             return this;
         };
         this.status = function () {
-            console.info('status: ', reference);
+            console.info('Alfirm Status!', alfirm);
         };
     }
-    
-    function /* constructor */ Confirm() {
-        /* functional inheritance */
-        Alert.apply(this, arguments);
-        
-        this.show = function () {
-            console.log('confirm show');
-            console.log(this.reference);
-        }
-    }
-    
+
     utils.shield = (function () {
         var counter = 0,
             store   = {};
-        
+
         return {
             getNewInstance: function () {
                 var instance = new Shield();
-                
+
                 store[++counter] = instance;
-                
+
                 return instance;
             },
-            status        : function () {
+            status:         function () {
                 console.info('instances: ', counter);
                 console.info('store: ', store);
             }
         }
     })();
-    utils.alert = (function () {
+    utils.alfirm = (function () {
         var instance;
-        
+
         return {
             getInstance: function () {
                 /* singleton */
                 if (!instance) {
-                    instance = new Alert();
+                    instance = new Alfirm();
                 }
-                
+
                 return instance;
             }
         }
     })();
-    utils.confirm = (function () {
-        var instance;
-        
-        return {
-            getInstance: function () {
-                /* singleton */
-                if (!instance) {
-                    instance = new Confirm();
-                }
-                
-                return instance;
-            }
-        }
-    })();
-    
+
     /* return */
     window[referenceName] = utils;
 })('rh_utils');
--- projects/rh_utils/js/rh_utils_alert.js
+++ projects/rh_utils/js/rh_utils_alert.js
@@ -4,9 +4,8 @@
  */
 
 (function () {
-    var rhAlert       = window['rh_utils'].alert.getInstance(),
-        rhConfirm     = window['rh_utils'].confirm.getInstance(),
-        buttons       = $('#x-controls').find('.btn-group').find('button'),
+    var alfirm        = window['rh_utils'].alfirm.getInstance(),
+        controls      = $('.rh-controls').find('button'),
         setTitle      = $('#set-title'),
         setMessage    = $('#set-message'),
         messages      = [
@@ -94,57 +93,57 @@
         ],
         randomInteger = function (min, max) {
             var rand = min - 0.5 + Math.random() * (max - min + 1);
-        
+
             return Math.round(rand);
         };
-    
-    buttons.eq(0).on('click', function (e) {
+
+    controls.eq(0).on('click', function (e) {
         window.alert(messages[randomInteger(0, (messages.length - 1))]);
     });
-    
-    buttons.eq(1).on('click', function (e) {
+
+    controls.eq(1).on('click', function (e) {
         var userReply = window.confirm(messages[randomInteger(0, (messages.length - 1))]);
-        
+
         if (userReply) {
             alert('OK');
         } else {
             alert('Cancel or ESC');
         }
     });
-    
-    buttons.eq(2).on('click', function (e) {
-        var message = setMessage.find('input').val();
-        
-        if (!message || (message && !message.length)) {
-            message = messages[randomInteger(0, (messages.length - 1))];
-        }
-        
-        rhAlert.create().message(message).show().status();
+
+    controls.eq(2).on('click', function (e) {
+        // var message = setMessage.find('input').val();
+        //
+        // if (!message || (message && !message.length)) {
+        //     message = messages[randomInteger(0, (messages.length - 1))];
+        // }
+
+        alfirm.show().status();
     });
-    
-    buttons.eq(3).on('click', function (e) {
-        rhConfirm.show();
-    });
-    
-    setTitle.find('button').first().on('click', function (e) {
-        rhAlert.title(setTitle.find('input').val());
-    });
-    setTitle.find('button').last().on('click', function (e) {
-        setTitle.find('input').val('');
-        rhAlert.title(null);
-    });
-    
-    setMessage.find('button').first().on('click', function (e) {
-        var message = setMessage.find('input').val();
-        
-        if (!message || (message && !message.length)) {
-            message = messages[randomInteger(0, (messages.length - 1))];
-        }
-        
-        rhAlert.message(message);
-    });
-    setMessage.find('button').last().on('click', function (e) {
-        rhAlert.message(null);
-        setMessage.find('input').val('');
-    });
+
+    // controls.eq(3).on('click', function (e) {
+    //     rhConfirm.show();
+    // });
+    //
+    // setTitle.find('button').eq(0).on('click', function (e) {
+    //     rhAlert.title(setTitle.find('input').val());
+    // });
+    // setTitle.find('button').eq(1).on('click', function (e) {
+    //     setTitle.find('input').val('');
+    //     rhAlert.title(null);
+    // });
+    //
+    // setMessage.find('button').eq(0).on('click', function (e) {
+    //     var message = setMessage.find('input').val();
+    //
+    //     if (!message || (message && !message.length)) {
+    //         message = messages[randomInteger(0, (messages.length - 1))];
+    //     }
+    //
+    //     rhAlert.message(message);
+    // });
+    // setMessage.find('button').eq(1).on('click', function (e) {
+    //     rhAlert.message(null);
+    //     setMessage.find('input').val('');
+    // });
 })();
