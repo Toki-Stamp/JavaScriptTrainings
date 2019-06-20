3a4f6ef5458afc8bedc93a0884760f925bf69449
Yuri Fomichev <fomichev@nca.by>
2019-06-20 15:32:56 +0300

    vue-component?

--- projects/vue-track-tree/js/vue-track-tree-script.js
+++ projects/vue-track-tree/js/vue-track-tree-script.js
@@ -4,52 +4,61 @@
  */
 
 let app = new Vue({
-    el     : '#vue-app',
-    data   : {
-        id   : null,
-        list : null,
-        chars: null
+    el:      '#vue-app',
+    data:    {
+        id:   null,
+        list: null
     },
-    watch  : {
-        id   : function (current, previous) {
+    watch:   {
+        id:   function (current, previous) {
             console.log('Watch data.id...');
             console.log({current: current, previous: previous});
         },
-        list : function (current, previous) {
+        list: function (current, previous) {
             console.log('Watch data.list...');
             console.log({current: current, previous: previous});
         },
-        chars: function (current, previous) {
-            console.log('Watch data.chars...');
-            console.log({current: current, previous: previous});
-        }
     },
     methods: {
-        getList : function () {
+        getList:  function () {
             this.$set(this, 'list', (function (data) {
                 let object = {};
-                
+
                 data.forEach(function (item) {
                     object[item.id] = item;
                 });
-                
+
                 return object;
             })(storage.list));
         },
         getChars: function (id) {
-            this.$set(this, 'chars', (storage.chars[id] ? storage.chars[id] : null));
-            
+            (storage.chars[id] && (this.list[id].characteristics = storage.chars[id]));
+
             $('#bootstrap-modal').modal('show');
         },
-        setID   : function (id) {
+        setID:    function (id) {
             this.$set(this, 'id', id);
         },
         highlight(event) {
-            $(event.target).parent().addClass('bg-primary text-light').siblings().removeClass('bg-primary text-light');
+            $(event.target).parent().addClass('table-primary').siblings().removeClass('table-primary');
         }
     },
     mounted: function () {
         $('#bootstrap-modal').modal({backdrop: false, keyboard: true, focus: true, show: false});
         console.log('Mounted Hook!');
+    }
+});
+
+Vue.component('characteristics', {
+    props:    ['characteristics'],
+    template: '<div>chars</div>',
+    watch:    {
+        characteristics: {
+            handler: function (current, previous) {
+                console.log('Watch characteristics...');
+                console.log({current: current, previous: previous});
+            },
+            deep:    true
+        }
     }
 });
