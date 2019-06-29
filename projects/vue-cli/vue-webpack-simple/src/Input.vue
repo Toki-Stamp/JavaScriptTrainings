c1c0d9e08ca19e9fbad61de7b748d9da97e2975c
fomichev <fomichev@nca.by>
2019-06-29 23:58:19 +0300

    vue-cli initiated!

---
+++ projects/vue-cli/vue-webpack-simple/src/Input.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <input type="text" v-model="text">
+        <p>Output: {{text}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Input",
+        data() {
+            return {
+                text: ''
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
