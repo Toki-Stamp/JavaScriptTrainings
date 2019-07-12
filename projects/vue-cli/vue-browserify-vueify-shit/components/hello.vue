609216c795324094885160b41b1ac92273b0af60
Yuri Fomichev <fomichev@nca.by>
2019-07-12 16:32:39 +0300

    vue browserify vueify webpack vue-loader

---
+++ projects/vue-cli/vue-browserify-vueify-shit/components/hello.vue
@@ -0,0 +1,17 @@
+<template>
+	<h1>{{greetings}}</h1>
+</template>
+
+<script>
+	module.exports = {
+		data: {
+			greetings: 'Hello'
+		}
+	}
+</script>
+
+<style>
+	h1 {
+		color: green;
+	}
+</style>
---
+++ projects/vue-cli/vue-webpack-loader/components/hello-again.vue
@@ -0,0 +1,19 @@
+<template>
+	<h1>{{greeting}}</h1>
+</template>
+
+<script>
+	module.exports = {
+		data: function () {
+			return {
+				greeting: 'Hello from Component!'
+			}
+		}
+	}
+</script>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
