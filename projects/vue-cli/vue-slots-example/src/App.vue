01e93cfecb9560f7e72d86d51a88bb549280d771
Yuri Fomichev <fomichev@nca.by>
2019-07-19 14:17:24 +0300

    slots dynamic components

---
+++ projects/vue-cli/vue-slots-example/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div id="controls">
+            <button v-on:click="changeComponentType('form-one')">Form One</button>
+            <button v-on:click="changeComponentType('form-two')">Form Two</button>
+        </div>
+        <keep-alive>
+            <component v-bind:is="componentType"></component>
+        </keep-alive>
+    </div>
+</template>
+
+<script>
+    import FormOne from './components/FormOne'
+    import FormTwo from './components/FormTwo'
+
+    export default {
+        name      : 'app',
+        components: {
+            'form-one': FormOne,
+            'form-two': FormTwo
+        },
+        data() {
+            return {
+                componentType: 'form-one'
+            }
+        },
+        methods   : {
+            changeComponentType(componentType) {
+                (this.componentType != componentType) && (this.componentType = componentType);
+            }
+        }
+    }
+</script>
+
+<style>
+    *, *:before, *:after {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
+</style>
---
+++ projects/vue-cli/vue-slots-example/src/components/FormHelper.vue
@@ -0,0 +1,63 @@
+<template>
+    <div id="form-helper">
+        <h1>Form Helper Component</h1>
+        <form>
+            <div id="form-header">
+                <slot name="form-header"></slot>
+            </div>
+            <div id="form-data">
+                <slot name="form-data"></slot>
+            </div>
+            <div id="form-controls">
+                <slot name="form-controls"></slot>
+            </div>
+            <div id="form-addendum">
+                <h3>This is form addendum</h3>
+                <ul>
+                    <li><a href="#">Link1</a></li>
+                    <li><a href="#">Link2</a></li>
+                    <li><a href="#">Link3</a></li>
+                    <li><a href="#">Link4</a></li>
+                </ul>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "FormHelper"
+    }
+</script>
+
+<style scoped>
+    #form-helper {
+        color: rgb(0, 91, 150);
+        border: 2px solid rgb(3, 57, 108);
+        border-radius: 4px;
+        background-color: rgb(179, 205, 224);
+        padding: 4px;
+        margin: 4px;
+    }
+
+    form > div:not(:empty) {
+        border: 2px solid rgb(3, 57, 108);
+        border-radius: 4px;
+        padding: 4px;
+        margin: 4px 0;
+    }
+
+    h1 {
+        text-align: center;
+    }
+
+    ul {
+        list-style: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin-right: 4px;
+    }
+</style>
