7cf4adb50fb93e6720c97df2d8a0e9b6cd19c45f
fomichev <fomichev@nca.by>
2019-07-19 23:42:03 +0300

    big refactoring here!

--- projects/vue-cli/webpack-simple-example/src/main.js
+++ projects/vue-cli/webpack-simple-example/src/main.js
@@ -3,6 +3,6 @@ import ROOT from './ROOT.vue';
 import '../../../../_lib/new/bootstrap-4.3.1/css/bootstrap.min.css';
 
 new Vue({
-    el    : '#root',
+    el    : '#app',
     render: h => h(ROOT)
 });
