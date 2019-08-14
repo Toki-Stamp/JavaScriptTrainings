73b3b955b1b97f467d2e2e4ed558634b910cbf57
Yuri Fomichev <fomichev@nca.by>
2019-08-14 16:16:15 +0300

    scanner locker

--- projects/rh_utils/js/test/rh_utils_test_scanner.js
+++ projects/rh_utils/js/test/rh_utils_test_scanner.js
@@ -4,14 +4,83 @@
  */
 
 (function sif() {
-    var content = $("#mainTableCont");
+    const
+        content = $("#mainTableCont"),
+        tr = content.find('tbody tr'),
+        buttonsContainer = $('.buttons'),
+        buttons = buttonsContainer.find('button');
 
-    content.on('click', 'tbody tr', function (e) {
+    let
+        debug = true,
+        backup = {};
+
+    function dummy(e) {
+        console.log('dummy', e);
+        e.stopImmediatePropagation();
+
+        return false;
+    }
+
+    function handler(e) {
         const self = $(this);
 
-        self.addClass('success').siblings().removeClass('success');
-        console.log({event: e, 'this': self});
-    });
+        console.log('handler-1', e.type, {event: e, 'this': self});
+    }
 
-    console.log(jQuery._data(content.get(0), 'events'));
+    function getEvents(target, type, deep) {
+        function recursion() {}
+
+        const all = jQuery._data(target.get(0), 'events');
+
+        return (type && type.length && all[type] ? all[type] : all);
+    }
+
+    function lock(events) {
+        function dummy() {/*dummy*/}
+
+        events.forEach(function (event) {
+            (!backup[event.guid]) && (backup[event.guid] = jQuery.extend(true, {}, event));
+            event.handler = dummy;
+        });
+    }
+
+    function lock_alt(target, type) {
+        var thisTarget = (target.get(0) || document.querySelector('body'));
+
+        thisTarget.addEventListener(type, dummy, true);
+    }
+
+    function unlock(events) {
+        if (backup && !jQuery.isEmptyObject(backup)) {
+            events.forEach(function (event) {
+                event.handler = backup[event.guid].handler;
+            });
+            backup = {};
+        }
+    }
+
+    function unlock_alt(target, type) {
+        var thisTarget = (target.get(0) || document.querySelector('body'));
+
+        thisTarget.removeEventListener(type, dummy, true);
+    }
+
+    content.on('click', 'tbody tr', handler);
+    tr.on('click', function (e) {
+        $(this).addClass('success').siblings().removeClass('success');
+    });
+    buttons.eq(0).click(function () {
+        console.log('locked');
+        buttonsContainer.add(buttonsContainer.parent()).addClass('locked');
+        // lock(getEvents(content, 'click'));
+        lock_alt(content, 'click');
+        $(this).attr('disabled', 'disabled').siblings().removeAttr('disabled');
+    });
+    buttons.eq(1).click(function () {
+        console.log('unlocked');
+        buttonsContainer.add(buttonsContainer.parent()).removeClass('locked');
+        // unlock(getEvents(content, 'click'));
+        unlock_alt(content, 'click');
+        $(this).attr('disabled', 'disabled').siblings().removeAttr('disabled');
+    });
 })();
