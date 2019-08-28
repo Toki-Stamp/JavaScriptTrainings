831e29917324bc63d026c8ddc3e4279b6b400320
fomichev <fomichev@nca.by>
2019-08-28 21:20:59 +0300

    except kind of...

--- projects/rh_utils/js/dev/rh_utils_dev_halt.js
+++ projects/rh_utils/js/dev/rh_utils_dev_halt.js
@@ -4,10 +4,10 @@
  */
 
 (function init_utils(referenceName) {
-    var utils = (window[referenceName] || {}),
-        debug = true,
+    var utils       = (window[referenceName] || {}),
+        debug       = true,
         greetingMsg = '... rh_utils successfully loaded!';
-
+    
     function /* constructor */ Halt(instance) {
         var defaultValues = {
                 target: $('body'),
@@ -54,76 +54,120 @@
                     "touchstart"
                 ]
             },
-            halt = {
+            halt          = {
                 target: defaultValues.target,
                 types : defaultValues.types,
-                except: null,
-                init  : false
+                except: {keyCodes: null, selector: null, object: null},
+                init  : false,
+                type  : null
             };
-
+        
+        // function handler(e) {
+        //     var me = $(e.target);
+        //
+        //     if (halt.except && me.is(halt.except)) {
+        //         (debug) && console.log('Halt: Pass Event Through', {id: instance.id, type: e.type, e: e, me: me});
+        //     } else {
+        //         (debug) && console.log('Halt: Block Event Propagation', {id: instance.id, type: e.type, e: e, me: me});
+        //         e.stopImmediatePropagation();
+        //
+        //         return false;
+        //     }
+        // }
+        
         function handler(e) {
             var me = $(e.target);
-
+            
             if (halt.except && me.is(halt.except)) {
                 (debug) && console.log('Halt: Pass Event Through', {id: instance.id, type: e.type, e: e, me: me});
             } else {
                 (debug) && console.log('Halt: Block Event Propagation', {id: instance.id, type: e.type, e: e, me: me});
                 e.stopImmediatePropagation();
-
+                
                 return false;
             }
         }
-
+        
         function init(description) {
             (debug) && console.log('Halt: Init', {id: instance.id});
-
+            
             (description.target && description.target.length && (description.target instanceof jQuery)) && (halt.target = description.target);
             (description.types && description.types.length && jQuery.isArray(description.types)) && (halt.types = description.types);
             (description.except && description.except.length && (description.except instanceof jQuery)) && (halt.except = description.except);
-
+            
             halt.init = true;
         }
-
+        
+        // this.lock = function (description) {
+        //     (!halt.init) && (init(description));
+        //     (debug) && console.log('Halt: Lock', {id: instance.id});
+        //
+        //     halt.types.forEach(function (type) {
+        //         halt.target.each(function () {
+        //             this.addEventListener(type, handler, true);
+        //         })
+        //     });
+        //
+        //     (instance && !instance.locked) && (instance.locked = true);
+        //
+        //     return this;
+        // };
         this.lock = function (description) {
             (!halt.init) && (init(description));
             (debug) && console.log('Halt: Lock', {id: instance.id});
-
+            
+            if (halt.type) {
+                if (halt.type === 'mouse') {
+                    halt.types = [];
+                } else if (halt.type === 'keyboard') {
+                    halt.types = ['keyup', 'keydown', 'keypress'];
+                }
+            }
+            
             halt.types.forEach(function (type) {
                 halt.target.each(function () {
                     this.addEventListener(type, handler, true);
                 })
             });
-
+            
             (instance && !instance.locked) && (instance.locked = true);
-
+            
             return this;
         };
         this.unlock = function () {
             (!halt.init) && (init({}));
             (debug) && console.log('Halt: Unlock', {id: instance.id});
-
+            
             halt.types.forEach(function (type) {
                 halt.target.each(function () {
                     this.removeEventListener(type, handler, true);
                 })
             });
-
+            
             (instance && instance.locked) && (instance.locked = false);
             (halt.init) && (halt.init = false);
-
+            
             return this;
-        }
+        };
+        
+        this.type = function (type) {
+            if (type) {
+                halt.type = type;
+            }
+        };
+        this.except = function (what) {
+        };
     }
-
+    
     utils.halt = (function haltFactory() {
         var counter = 0,
             storage = {};
-
+        
         return {
             getInstance: function () {
-                var id = ++counter,
+                var id    = ++counter,
                     state = false,
-                    item = Object.create({locked: state}, {
+                    item  = Object.create({locked: state}, {
                         id    : {
                             /* non mutable */
                             value: id
@@ -133,21 +177,21 @@
                                 (debug) && (console.log('Halt Factory: Setting Lock State', {
                                     id: id, from: state, to: value
                                 }));
-
+                            
                                 state = value;
                             },
                             get: function () {
                                 (debug) && (console.log('Halt Factory: Getting Lock State', {id: id, locked: state}));
-
+                            
                                 return state;
                             }
                         }
                     });
-
+                
                 Object.defineProperty(item, 'ref', {value: new Halt(item)});
-
+                
                 storage[id] = item;
-
+                
                 return item.ref;
             },
             status     : function () {
--- projects/rh_utils/js/test/rh_utils_test_halt.js
+++ projects/rh_utils/js/test/rh_utils_test_halt.js
@@ -4,55 +4,60 @@
  */
 
 (function sif() {
-    var content = $("#mainTableCont"),
-        tr = content.find('tbody tr'),
+    var content          = $("#mainTableCont"),
+        tr               = content.find('tbody tr'),
         buttonsContainer = $('.buttons'),
-        buttons = buttonsContainer.find('button'),
-        backup = {};
+        buttons          = buttonsContainer.find('button'),
+        backup           = {};
     var halt = window['rh_utils'].halt.getInstance();
-
+    var keyCodes = {
+        up  : 38,
+        down: 40,
+        esc : 27
+    };
+    
     function dummy(e) {
         console.log('dummy', e);
         e.stopImmediatePropagation();
-
+        
         return false;
     }
-
+    
     function handler(e) {
         const self = $(this);
-
+        
         console.log('handler-1', e.type, {event: e, 'this': self});
     }
-
+    
     function handler2(e) {
         const self = $(this);
-
+        
         console.log('handler-2', e.type, {event: e, 'this': self});
     }
-
+    
     function handler3(e) {
         const self = $(this);
-
+        
         console.log('handler-3', e.type, {event: e, 'this': self});
     }
-
+    
     function getEvents(target, type, deep) {
         function recursion() {}
-
+        
         const all = jQuery._data(target.get(0), 'events');
-
+        
         return (type && type.length && all[type] ? all[type] : all);
     }
-
+    
     function lock(events) {
         function dummy() {/*dummy*/}
-
+        
         events.forEach(function (event) {
             (!backup[event.guid]) && (backup[event.guid] = jQuery.extend(true, {}, event));
             event.handler = dummy;
         });
     }
-
+    
     function unlock(events) {
         if (backup && !jQuery.isEmptyObject(backup)) {
             events.forEach(function (event) {
@@ -61,21 +66,48 @@
             backup = {};
         }
     }
-
+    
+    function highlight(target) {
+        target.addClass('success').siblings().removeClass('success');
+    }
+    
     content.on('click', 'tbody tr', handler);
     content.on('dblclick', 'tbody tr', handler2);
     content.on('contextmenu', 'tbody tr', handler3);
+    $(document).on('keydown', function (e) {
+        var current = tr.filter('.success');
+        
+        // e.preventDefault();
+        
+        if (e.keyCode === keyCodes.up) {
+            if (current && !current.length) {
+                current = tr.first();
+            } else {
+                current = current.prev();
+            }
+        } else if (e.keyCode === keyCodes.down) {
+            if (current && !current.length) {
+                current = tr.last();
+            } else {
+                current = current.next();
+            }
+        }
+        
+        highlight(current);
+    });
     tr.on('click', function (e) {
-        $(this).addClass('success').siblings().removeClass('success');
+        highlight($(this));
     });
     buttons.eq(0).click(function () {
         buttonsContainer.add(buttonsContainer.parent()).addClass('locked');
         $(this).attr('disabled', 'disabled').siblings().removeAttr('disabled');
-        halt.lock({
-            target: null,
-            types : ['click', 'dblclick', 'contextmenu'],
-            except: buttons
-        });
+        // halt.lock({
+        //     target: null,
+        //     types : ['click', 'dblclick', 'contextmenu'],
+        //     except: buttons
+        // });
+        
+        halt.except({keyCodes: [27]}).lock();
     });
     buttons.eq(1).click(function () {
         buttonsContainer.add(buttonsContainer.parent()).removeClass('locked');
