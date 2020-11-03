24bda5cf46a08255f63475bf236526d9ee1d37f2
fomichev <fomichev@nca.by>
2020-11-03 23:43:37 +0300

    Localize -> Record details

--- projects/vue-cli/crm/src/views/DetailRecord.vue
+++ projects/vue-cli/crm/src/views/DetailRecord.vue
@@ -5,38 +5,42 @@
       <div class="breadcrumb-wrap">
         <router-link
           to="/history"
-          class="breadcrumb">История
+          class="breadcrumb">{{'title_history' | localizeFilter}}
         </router-link>
-        <a class="breadcrumb" @click.prevent>
-          {{record.type === 'outcome' ? 'Расход' : 'Доход'}}
-        </a>
+        <a class="breadcrumb" @click.prevent>{{breadcrumb}}</a>
       </div>
       <div class="row">
         <div class="col s12">
           <div class="card"
                :class="{'red': (record.type === 'outcome'), 'green': (record.type === 'income')}">
             <div class="card-content white-text">
-              <p>Описание: {{record.description}}</p>
-              <p>Сумма: {{record.amount | currencyFilter}}</p>
-              <p>Категория: {{record.categoryName}}</p>
-
+              <p>{{'title_description' | localizeFilter}}: {{record.description}}</p>
+              <p>{{'sum' | localizeFilter}}: {{record.amount | currencyFilter}}</p>
+              <p>{{'category' | localizeFilter}}: {{record.categoryName}}</p>
               <small>{{record.date | dateFilter('datetime')}}</small>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <p class="center" v-else>Запись с ID=<strong style="color: red">{{$route.params.recordID}}</strong> не найдена</p>
+    <p class="center" v-else>{{'message_record_not_found' | localizeFilter}}: <strong style="color: red">{{$route.params.recordID}}</strong></p>
   </div>
 </template>
 
 <script>
+  import localizeFilter from '../filters/localize-filter.js';
+
   export default {
     name: 'DetailRecord',
     data: () => ({
       record: null,
       loading: true
     }),
+    computed: {
+      breadcrumb() {
+        return (this.record.type === 'outcome') ? localizeFilter('outcome') : localizeFilter('income');
+      }
+    },
     async mounted() {
       const recordID = this.$route.params.recordID;
       const record = await this.$store.dispatch('fetchRecord', recordID);
